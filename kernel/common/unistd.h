5f24cd56fe33b809e76936e7908f7efc6b84a0e1
fslongjin <fslongjin@vip.qq.com>
2022-04-22 23:15:47 +0800

    :new: errno.h and unistd.h

---
+++ kernel/common/unistd.h
@@ -0,0 +1,14 @@
+/**
+ * @file unistd.h
+ * @author fslongjin (longjin@RinGoTek.cn)
+ * @brief
+ * @version 0.1
+ * @date 2022-04-22
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#pragma once
+
+#include <syscall/syscall.h>
+#include <syscall/syscall_num.h>

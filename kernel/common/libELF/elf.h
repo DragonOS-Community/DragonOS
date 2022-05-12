46b08c7738bc4ef1d04f1dd7d137ae923e3c5fb2
fslongjin <fslongjin@vip.qq.com>
2022-05-12 23:45:39 +0800

    add some datastructure

---
+++ kernel/common/libELF/elf.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <common/glib.h>
+
+// ====== ELF头文件中的
+typedef uint32_t Elf32_Addr;
+typedef uint16_t Elf32_Half;
+typedef uint32_t Elf32_Off;
+typedef uint32_t Elf32_SWord;
+typedef uint32_t Elf32_Word;
+

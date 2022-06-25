0d6957294d54553b1bb2fa41575c5842180fcfcb
fslongjin <fslongjin@vip.qq.com>
2022-06-25 20:57:49 +0800

    消除了libc的_start方法的编译警告

--- user/libs/libc/sysdeps/x86_64/elf/start.c
+++ user/libs/libc/sysdeps/x86_64/elf/start.c
@@ -1,10 +1,13 @@
 
-extern int main(int, char**);
-#include<libc/stdio.h>
-void _start(int argc, char** argv)
+#include <libc/stdio.h>
+#include <libc/stdlib.h>
+
+extern int main(int, char **);
+
+void _start(int argc, char **argv)
 {
-    printf("before main\n");
+    // printf("before main\n");
     int retval = main(argc, argv);
-    printf("before exit, code=%d\n", retval);
+    // printf("before exit, code=%d\n", retval);
     exit(retval);
 }

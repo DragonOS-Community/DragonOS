900f18653046d2fbbc888150d84bf0ec9f9c78b2
fslongjin <fslongjin@vip.qq.com>
2022-07-07 23:12:09 +0800

    修改磁盘脚本的细节

--- tools/mount_virt_disk.sh
+++ tools/mount_virt_disk.sh
@@ -2,7 +2,7 @@ LOOP_DEVICE=$(sudo losetup -f --show -P ../bin/disk.img) \
     || exit 1
 
 echo ${LOOP_DEVICE}p1
-sudo mkfs.vfat -F 32 ${LOOP_DEVICE}p1
+
 mkdir -p ../bin/disk_mount/
 sudo mount ${LOOP_DEVICE}p1 ../bin/disk_mount/ 
 lsblk

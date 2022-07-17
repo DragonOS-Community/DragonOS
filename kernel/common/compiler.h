a1c843cd3a4acc8e7dee60390bbace4196792dc2
fslongjin <fslongjin@vip.qq.com>
2022-07-17 17:23:56 +0800

    new: 简单的warn assert

---
+++ kernel/common/compiler.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
+#ifndef barrier
+// 内存屏障
+#define barrier() __asm__ __volatile__("" :: \
+                                           : "memory");
+#endif
---
+++ kernel/debug/bug.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <common/compiler.h>
+#include <common/kprint.h>
+
+/**
+ * @brief 当condition为true时输出警告信息
+ * 
+ */
+#define WARN_ON(condition) ({                                   \
+    int __ret_warn_on = !!(condition);                          \
+    if (unlikely(__ret_warn_on))                                \
+        kwarn("Assertion failed at %s:%d", __FILE__, __LINE__); \
+    unlikely(__ret_warn_on);                                    \
+})

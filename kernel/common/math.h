bf4226f6b924f8df0c7905f590971ab027c0ae21
fslongjin <longjin@RinGoTek.cn>
2022-08-18 23:29:51 +0800

    new: 统计前导0

--- kernel/common/math.h
+++ kernel/common/math.h
@@ -1,3 +1,10 @@
 #pragma once
 #include "stddef.h"
+#include <arch/arch.h>
+#if ARCH(I386) || ARCH(X86_64)
+#include <arch/x86_64/math/bitcount.h>
+#else
+#error Arch not supported.
+#endif
+
 int64_t pow(int64_t x, int y);

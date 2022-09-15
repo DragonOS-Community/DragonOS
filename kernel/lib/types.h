64578c72c658f7bc85018b129b9e4b4775f7a36b
pyrotechnicdemon <handsomepd@qq.com>
2022-09-15 21:09:50 +0800

    crc

---
+++ kernel/lib/types.h
@@ -0,0 +1,12 @@
+#ifndef __TYPE_H__
+#define __TYPE_H__
+#include <stdio.h>
+ //和 Linux 命名的差不多 
+typedef unsigned char        u8;
+typedef unsigned short       u16;
+typedef unsigned int         u32;
+typedef unsigned long long   u64;
+
+
+
+#endif

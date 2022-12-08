2d5a9542f1341de5f357a934444ddff6d8473ca9
yujianwu <yujianwu2019@gmail.com>
2022-12-09 00:41:29 +0800

    uefi修改

--- tools/run-qemu.sh
+++ tools/run-qemu.sh
@@ -2,6 +2,7 @@
 flag_can_run=1
 ARGS=`getopt -o p -l bios: -- "$@"`
 eval set -- "${ARGS}"
+echo "$@"
 allflags=$(qemu-system-x86_64 -cpu help | awk '/flags/ {y=1; getline}; y {print}' | tr ' ' '\n' | grep -Ev "^$" | sed -r 's|^|+|' | tr '\n' ',' | sed -r "s|,$||")
 
 # 请根据自己的需要，在-d 后方加入所需的trace事件
@@ -14,28 +15,33 @@ qemu_accel=kvm
 if [ $(uname) == Darwin ]; then
     qemu_accel=hvf
 fi
-QEMU= qemu-system-x86_64
-QEMU_ARGUMENT := -d ../bin/disk.img -m 512M -smp 2,cores=2,threads=1,sockets=1 \
--boot order=d   \
--monitor stdio -d ${qemu_trace_std} \
--s -S -cpu IvyBridge,apic,x2apic,+fpu,check,${allflags} -rtc clock=host,base=localtime -serial file:../serial_opt.txt \
--drive id=disk,file=../bin/disk.img,if=none \
--device ahci,id=ahci \
--device ide-hd,drive=disk,bus=ahci.0    \
--net nic,model=virtio \
--usb    \
--device qemu-xhci,id=xhci,p2=8,p3=4 \
--machine accel=${qemu_accel} 
+QEMU=qemu-system-x86_64
+QEMU_DISK_IMAGE="../bin/disk.img"
+QEMU_MEMORY="512M"
+QEMU_SMP="2,cores=2,threads=1,sockets=1"
+QEMU_MONITOR="stdio"
+QEMU_TRACE="${qemu_trace_std}"
+QEMU_CPU_FEATURES="IvyBridge,apic,x2apic,+fpu,check,${allflags}"
+QEMU_RTC_CLOCK="clock=host,base=localtime"
+QEMU_SERIAL="file:../serial_opt.txt"
+QEMU_DRIVE="id=disk,file=${QEMU_DISK_IMAGE},if=none"
+
+QEMU_DEVICES="-device ahci,id=ahci -device ide-hd,drive=disk,bus=ahci.0 -net nic,model=virtio -usb -device qemu-xhci,id=xhci,p2=8,p3=4 -machine accel=${qemu_accel}"
+
+QEMU_ARGUMENT="-d ${QEMU_DISK_IMAGE} -m ${QEMU_MEMORY} -smp ${QEMU_SMP} -boot order=d -monitor ${QEMU_MONITOR} -d ${qemu_trace_std} "
+
+QEMU_ARGUMENT+="-s -S -cpu ${QEMU_CPU_FEATURES} -rtc ${QEMU_RTC_CLOCK} -serial ${QEMU_SERIAL} -drive ${QEMU_DRIVE} ${QEMU_DEVICES}"
+
 if [ $flag_can_run -eq 1 ]; then
-    case "$1" in
+     case "$1" in
         --bios) 
      case "$2" in
             uefi) #uefi启动新增ovmf.fd固件
-             $(QEMU) -bios arch/X86_64/OVMF.fd $(QEMU_ARGUMENT)
+             ${QEMU} -bios arch/X86_64/OVMF.fd ${QEMU_ARGUMENT}
            ;;
             legacy)
-             $(QEMU) $(QEMU_ARGUMENT)
-           esac       
+             ${QEMU} ${QEMU_ARGUMENT}
+             esac
 esac  
 else
   echo "不满足运行条件"

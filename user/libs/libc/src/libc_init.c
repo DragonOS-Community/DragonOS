05eeb044c6eb4c21fe23c0a2f12105f2bd9b9dbd
guanjinquan <1666320330@qq.com>
2023-01-05 17:15:31 +0800

    添加了stderr的初始化

--- user/libs/libc/src/libc_init.c
+++ user/libs/libc/src/libc_init.c
@@ -1,4 +1,14 @@
+#include <stdio.h>
+
+FILE* stdin = {0};
+FILE* stdout = {0};
+FILE* stderr = {0};
+
 void _libc_init()
 {
-    // TODO: 在这初始化stderr
+    // 初始化标准流对应的文件描述符
+    stdin->fd = 0;
+    stdout->fd = 1;
+    stderr->fd = 2;
+    
 }

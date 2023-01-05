067fffb20e3fdb1b1aa74fe3accbe9e5b608c83a
longjin <longjin@RinGoTek.cn>
2023-01-05 17:02:49 +0800

    应用程序启动前，调用初始化libc的函数

--- user/libs/libc/src/arch/x86_64/crt0.c
+++ user/libs/libc/src/arch/x86_64/crt0.c
@@ -4,11 +4,13 @@
 
 extern int main(int, char **);
 extern void _init();
+extern void _libc_init();
 
 void _start(int argc, char **argv)
 {
     // Run the global constructors.
     _init();
+    _libc_init();
     int retval = main(argc, argv);
     // printf("before exit, code=%d\n", retval);
     exit(retval);

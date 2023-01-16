f65b02734767c398c349d6c12dd89fe20990c778
login <longjin@ringotek.cn>
2023-01-16 20:17:16 +0800

    Patch merge master (#152)
    * Patch sched rust (#139)
    
    * update
    
    * 添加rt调度器的rust初步实现
    
    * 完善rt调度逻辑
    
    * 调试rt调度器
    
    * 修改sched的返回值
    
    * cargo fmt 格式化
    
    * 删除无用代码，修补rt bug
    
    * 删除无用的代码，和重复的逻辑
    
    * 软中断bugfix
    
    * 删除一些代码
    
    * 添加kthread_run_rt文档
    
    * 解决sphinix警告_static目录不存在的问题
    
    Co-authored-by: longjin <longjin@RinGoTek.cn>
    
    * Raw spin lock 增加lock_irqsave、unlock_irqrestore(#151)
    
    Raw spin lock 增加lock_irqsave、unlock_irqrestore
    
    Co-authored-by: kong <45937622+kkkkkong@users.noreply.github.com>
    Co-authored-by: Gou Ngai <94795048+AlbertSanoe@users.noreply.github.com>

--- kernel/src/libs/ffi_convert.rs
+++ kernel/src/libs/ffi_convert.rs
@@ -6,24 +6,10 @@ pub trait FFIBind2Rust<T> {
     fn convert_mut(src: *mut T) -> Option<&'static mut Self>;
 }
 
-
-
-pub fn __convert_mut<'a, S, D>(src:*mut S) ->Option<&'a mut D>{
-    return unsafe {
-        core::mem::transmute::<
-            *mut S,
-            *mut D,
-        >(src)
-        .as_mut()
-    };
+pub fn __convert_mut<'a, S, D>(src: *mut S) -> Option<&'a mut D> {
+    return unsafe { core::mem::transmute::<*mut S, *mut D>(src).as_mut() };
 }
 
-pub fn __convert_ref<'a, S, D>(src:*const S) ->Option<&'a D>{
-    return unsafe {
-        core::mem::transmute::<
-            *const S,
-            *const D,
-        >(src)
-        .as_ref()
-    };
+pub fn __convert_ref<'a, S, D>(src: *const S) -> Option<&'a D> {
+    return unsafe { core::mem::transmute::<*const S, *const D>(src).as_ref() };
 }

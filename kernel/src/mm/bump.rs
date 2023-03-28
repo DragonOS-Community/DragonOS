3593e896845292b968425a43177eb03a507de470
kong <376634352@qq.com>
2023-03-28 01:28:07 +0000

    修改BumpAllocator的分配逻辑

--- kernel/src/mm/bump.rs
+++ kernel/src/mm/bump.rs
@@ -1,4 +1,4 @@
-use crate::mm::page_frame::{FrameAllocator, PageFrameUsage, PageFrameCount};
+use crate::mm::page_frame::{FrameAllocator, PageFrameCount, PageFrameUsage};
 use crate::mm::{MemoryManagementArch, PhysAddr, PhysMemoryArea};
 use core::marker::PhantomData;
 // 线性分配器的实现
@@ -31,27 +31,39 @@ impl<MMA: MemoryManagementArch> BumpAllocator<MMA> {
 
 impl<MMA: MemoryManagementArch> FrameAllocator for BumpAllocator<MMA> {
     unsafe fn allocate(&mut self, count: PageFrameCount) -> Option<PhysAddr> {
-        //TODO: 支持分配多个页帧
-        // 目前只支持分配一个页帧，所以如果count.data不等于1，直接返回None
-        if count.data() != 1 {
-            return None;
-        }
-        let mut offset = self.offset;
+        let mut offset = self.offset();
         // 遍历所有的物理内存区域
-        for area in self.areas.iter() {
-            // 如果offset小于area.size，说明当前的物理内存区域还有空闲的页帧
-            if offset < area.size {
-                let page_phys = area.base.add(offset);
+        for area in self.areas().iter() {
+            // 将area的base地址与PAGE_SIZE对齐，对其时向上取整
+            let area_base = (area.base.data() + MMA::PAGE_SIZE - 1) & !(MMA::PAGE_SIZE - 1);
+            // 将area的末尾地址与PAGE_SIZE对齐，对其时向下取整
+            let area_end = (area.base.data() + area.size) & !(MMA::PAGE_SIZE - 1);
+
+            // 如果offset大于area_end，说明当前的物理内存区域已经分配完了，需要跳到下一个物理内存区域
+            if offset >= area_end {
+                continue;
+            }
+
+            // 如果offset小于area_base 或者不小于area_base但小于area_end，说明当前的物理内存区域还没有分配过页帧，需要将offset调整到area_base
+            if offset < area_base || offset < area_end {
+                offset = area_base;
+            }
+            // 如果当前offset到area_end的距离大于等于count.data() * PAGE_SIZE，说明当前的物理内存区域足以分配count个页帧
+            if offset + count.data() * MMA::PAGE_SIZE <= area_end {
+                let res_page_phys = offset;
+                let page_phys = area.base.add(count.data() * MMA::PAGE_SIZE);
+                // 将page_phys转换为虚拟地址
                 let page_virt = MMA::phys_2_virt(page_phys);
                 // 将page_virt从Option中取出来
                 let page_virt =
                     page_virt.expect("BumpAllocator::allocate: invalid physical address");
-                MMA::write_bytes(page_virt, 0, MMA::PAGE_SIZE);
-                self.offset += MMA::PAGE_SIZE;
-                return Some(page_phys);
+                // 将page_virt对应的物理页清零
+                MMA::write_bytes(page_virt, 0, count.data() * MMA::PAGE_SIZE);
+                // 将offset增加至分配后的内存
+                self.offset = offset + count.data() * MMA::PAGE_SIZE;
+
+                return Some(PhysAddr(res_page_phys));
             }
-            // 如果offset大于area.size，说明当前的物理内存区域已经分配完了，需要跳到下一个物理内存区域
-            offset -= area.size;
         }
         return None;
     }
@@ -63,10 +75,22 @@ impl<MMA: MemoryManagementArch> FrameAllocator for BumpAllocator<MMA> {
 
     unsafe fn usage(&self) -> PageFrameUsage {
         let mut total = 0;
-        for area in self.areas.iter() {
-            total += area.size >> MMA::PAGE_SHIFT;
+        let mut used = 0;
+        for area in self.areas().iter() {
+            // 将area的base地址与PAGE_SIZE对齐，对其时向上取整
+            let area_base = (area.base.data() + MMA::PAGE_SIZE - 1) & !(MMA::PAGE_SIZE - 1);
+            // 将area的末尾地址与PAGE_SIZE对齐，对其时向下取整
+            let area_end = (area.base.data() + area.size) & !(MMA::PAGE_SIZE - 1);
+
+            total += (area_end - area_base) >> MMA::PAGE_SHIFT;
+            // 如果offset大于area_end，说明当前物理区域被分配完，都需要加到used中
+            if self.offset >= area_end {
+                used += (area_end - area_base) >> MMA::PAGE_SHIFT;
+            }
+            else{
+                used += (self.offset - area_base) >> MMA::PAGE_SHIFT;
+            }
         }
-        let used = self.offset >> MMA::PAGE_SHIFT;
         let frame = PageFrameUsage::new(PageFrameCount::new(used), PageFrameCount::new(total));
         return frame;
     }

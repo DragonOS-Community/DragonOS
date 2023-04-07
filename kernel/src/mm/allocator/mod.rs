0de5deedc4ec54e0f5d310edf4f26acf701b2e4e
longjin <longjin@RinGoTek.cn>
2023-04-07 05:16:37 +0000

    解决无法通过编译的问题

--- kernel/src/mm/allocator/mod.rs
+++ kernel/src/mm/allocator/mod.rs
@@ -1,9 +1,4 @@
 pub mod buddy;
-pub mod buddy_bak;
-/// @Auther: Kong
-/// @Date: 2023-03-31 12:44:59
-/// @FilePath: /DragonOS/kernel/src/mm/allocator/mod.rs
-/// @Description:
 pub mod bump;
 pub mod c;
 pub mod page_frame;

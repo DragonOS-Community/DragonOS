dd9f1fc1a42406461e6f0d38cce1e56e22a1a15f
TingHuang <92705854+TingSHub@users.noreply.github.com>
2023-04-21 16:03:42 +0800

    新增SysFS (#250)
    * 添加sysfs
    
    * 注册sysfs
    
    * 添加sysfs相关
    
    * 添加rust-anlyzer辅助配置
    
    * 将设备与sysfs相关联
    
    * 添加单独的文件管理sysfs下的文件夹

---
+++ kernel/src/filesystem/sysfs/class.rs
@@ -0,0 +1,66 @@
+use super::{LockedSysFSInode, SYS_CLASS_INODE};
+use crate::{filesystem::vfs::IndexNode, syscall::SystemError};
+use alloc::sync::Arc;
+
+/// @brief: 注册class，在sys/class下生成文件夹
+/// @parameter class_name: 类文件夹名
+/// @return: 操作成功，返回inode，操作失败，返回错误码
+#[inline]
+#[allow(dead_code)]
+pub fn class_register(class_name: &str) -> Result<Arc<dyn IndexNode>, SystemError> {
+    let binding: Arc<dyn IndexNode> = SYS_CLASS_INODE();
+    binding
+        .as_any_ref()
+        .downcast_ref::<LockedSysFSInode>()
+        .ok_or(SystemError::E2BIG)
+        .unwrap()
+        .add_dir(class_name)
+}
+
+/// @brief: 注销class，在sys/class删除文件夹
+/// @parameter class_name: 总线文件夹名
+/// @return: 操作成功，返回()，操作失败，返回错误码
+#[inline]
+#[allow(dead_code)]
+pub fn class_unregister(class_name: &str) -> Result<(), SystemError> {
+    let binding: Arc<dyn IndexNode> = SYS_CLASS_INODE();
+    binding
+        .as_any_ref()
+        .downcast_ref::<LockedSysFSInode>()
+        .ok_or(SystemError::E2BIG)
+        .unwrap()
+        .remove(class_name)
+}
+
+/// @brief: 注册device，在对应类下操作设备文件夹
+/// @parameter class: 类文件夹inode
+/// @parameter device_name: 设备文件夹名
+/// @return: 操作成功，返回inode，操作失败，返回错误码
+#[inline]
+#[allow(dead_code)]
+pub fn device_register(
+    class: Arc<dyn IndexNode>,
+    device_name: &str,
+) -> Result<Arc<dyn IndexNode>, SystemError> {
+    class
+        .as_any_ref()
+        .downcast_ref::<LockedSysFSInode>()
+        .ok_or(SystemError::E2BIG)
+        .unwrap()
+        .add_dir(device_name)
+}
+
+/// @brief: 操作device，在对应类下删除设备文件夹
+/// @parameter class: 类文件夹inode
+/// @parameter device_name: 设备文件夹名
+/// @return: 操作成功，返回()，操作失败，返回错误码
+#[inline]
+#[allow(dead_code)]
+pub fn device_unregister(class: Arc<dyn IndexNode>, device_name: &str) -> Result<(), SystemError> {
+    class
+        .as_any_ref()
+        .downcast_ref::<LockedSysFSInode>()
+        .ok_or(SystemError::E2BIG)
+        .unwrap()
+        .remove(device_name)
+}

d1defa5cc37075365490d1bafb6766c1fda1cfb8
longjin <longjin@RinGoTek.cn>
2023-05-30 02:14:19 +0000

    新的ipi功能&kick_cpu功能的重写

---
+++ kernel/src/exception/ipi.rs
@@ -0,0 +1,21 @@
+#[allow(dead_code)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(u8)]
+pub enum IpiKind {
+    KickCpu,
+    FlushTLB,
+}
+
+/// IPI投递目标
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[allow(dead_code)]
+pub enum IpiTarget {
+    /// 当前CPU
+    Current,
+    /// 所有CPU
+    All,
+    /// 除了当前CPU以外的所有CPU
+    Other,
+    /// 指定的CPU
+    Specified(usize),
+}

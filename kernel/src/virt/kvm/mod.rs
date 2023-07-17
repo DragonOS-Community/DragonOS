598d482c23654ed692b36b477f5b30631e0d9585
xiaoyez <308169188@qq.com>
2023-07-17 15:53:45 +0800

    修改cr0,cr4,msr寄存器enable VMX operations

--- kernel/src/virt/kvm/mod.rs
+++ kernel/src/virt/kvm/mod.rs
@@ -1,5 +1,6 @@
 use core::arch::x86_64;
 use raw_cpuid::CpuId;
+use x86::{controlregs, msr};
 use crate::kdebug;
 use crate::filesystem::devfs::{DevFS, DeviceINode, devfs_register};
 pub use self::kvm_dev::LockedKvmInode;
@@ -25,6 +26,73 @@ pub fn has_intel_vmx_support() -> Result<(), SystemError> {
         }
     }
     Ok(())
+}
+
+/// Enables Virtual Machine Extensions 
+// - CR4.VMXE[bit 13] = 1 (Intel Manual: 24.7 Enabling and Entering VMX Operation)
+pub fn enable_vmx_operation() -> Result<(), SystemError> {
+    let mut cr4 = unsafe { controlregs::cr4() };
+    cr4.set(controlregs::Cr4::CR4_ENABLE_VMX, true);
+    unsafe { controlregs::cr4_write(cr4) };
+
+    set_lock_bit()?;
+    kdebug!("[+] Lock bit set via IA32_FEATURE_CONTROL");
+    set_cr0_bits();
+    kdebug!("[+] Mandatory bits in CR0 set/cleared");
+    set_cr4_bits();
+    kdebug!("[+] Mandatory bits in CR4 set/cleared");
+
+    Ok(())
+}
+
+/// Check if we need to set bits in IA32_FEATURE_CONTROL 
+// (Intel Manual: 24.7 Enabling and Entering VMX Operation)
+fn set_lock_bit() -> Result<(), SystemError> {
+    const VMX_LOCK_BIT: u64 = 1 << 0;
+    const VMXON_OUTSIDE_SMX: u64 = 1 << 2;
+
+    let ia32_feature_control = unsafe { msr::rdmsr(msr::IA32_FEATURE_CONTROL) };
+
+    if (ia32_feature_control & VMX_LOCK_BIT) == 0 {
+        unsafe {
+            msr::wrmsr(
+                msr::IA32_FEATURE_CONTROL,
+                VMXON_OUTSIDE_SMX | VMX_LOCK_BIT | ia32_feature_control,
+            )
+        };
+    } else if (ia32_feature_control & VMXON_OUTSIDE_SMX) == 0 {
+        return Err(SystemError::EPERM);
+    }
+
+    Ok(())
+}
+
+/// Set the mandatory bits in CR0 and clear bits that are mandatory zero 
+/// (Intel Manual: 24.8 Restrictions on VMX Operation)
+fn set_cr0_bits() {
+    let ia32_vmx_cr0_fixed0 = unsafe { msr::rdmsr(msr::IA32_VMX_CR0_FIXED0) };
+    let ia32_vmx_cr0_fixed1 = unsafe { msr::rdmsr(msr::IA32_VMX_CR0_FIXED1) };
+
+    let mut cr0 = unsafe { controlregs::cr0() };
+
+    cr0 |= controlregs::Cr0::from_bits_truncate(ia32_vmx_cr0_fixed0 as usize);
+    cr0 &= controlregs::Cr0::from_bits_truncate(ia32_vmx_cr0_fixed1 as usize);
+
+    unsafe { controlregs::cr0_write(cr0) };
+}
+
+/// Set the mandatory bits in CR4 and clear bits that are mandatory zero 
+/// (Intel Manual: 24.8 Restrictions on VMX Operation)
+fn set_cr4_bits() {
+    let ia32_vmx_cr4_fixed0 = unsafe { msr::rdmsr(msr::IA32_VMX_CR4_FIXED0) };
+    let ia32_vmx_cr4_fixed1 = unsafe { msr::rdmsr(msr::IA32_VMX_CR4_FIXED1) };
+
+    let mut cr4 = unsafe { controlregs::cr4() };
+
+    cr4 |= controlregs::Cr4::from_bits_truncate(ia32_vmx_cr4_fixed0 as usize);
+    cr4 &= controlregs::Cr4::from_bits_truncate(ia32_vmx_cr4_fixed1 as usize);
+
+    unsafe { controlregs::cr4_write(cr4) };
 }
 
 // struct Kvm {
@@ -80,6 +148,7 @@ pub extern "C" fn kvm_init() {
     //     panic!("Failed to register /dev/kvm");
     // }
     has_intel_vmx_support().expect("No Intel VMX support");
+    enable_vmx_operation().expect("Cannot enable vmx operation");
     devfs_register("kvm", LockedKvmInode::new())
         .expect("Failed to register /dev/kvm");
 }

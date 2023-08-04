6b16c5507b2a91c6b303a3dbeb1031889b246700
LoongGshine <gshine@m.scnu.edu.cn>
2023-08-04 13:56:41 +0800

    使用 rust 范式完成功能

--- kernel/src/notifier/notifier.rs
+++ kernel/src/notifier/notifier.rs
@@ -4,41 +4,29 @@ use crate::{
     syscall::SystemError,
 };
 use alloc::{sync::Arc, vec::Vec};
-use core::ffi::c_void;
 
-/// @brief 通知链中注册的回调函数类型
-type NotifierFnT = fn(Arc<NotifierBlock>, u64, *mut c_void) -> i32;
-
-#[derive(Debug)]
 /// @brief 通知链节点
-pub struct NotifierBlock {
-    notifier_call: Option<NotifierFnT>,
-    priority: i32,
-}
-
-impl NotifierBlock {
-    pub fn new(notifier_call: Option<NotifierFnT>, priority: i32) -> Self {
-        Self {
-            notifier_call,
-            priority,
-        }
-    }
+pub trait NotifierBlock<T> {
+    /// @brief 通知链中注册的回调函数类型
+    fn notifier_call(&self, action: u64, data: &T) -> i32;
+    /// @brief 通知链节点的优先级
+    fn priority(&self) -> i32;
 }
 
 /// @brief 通知链
-struct NotifierChain(Vec<Arc<NotifierBlock>>);
+// TODO: 考虑使用红黑树封装
+struct NotifierChain<T>(Vec<Arc<dyn NotifierBlock<T>>>);
 
-impl NotifierChain {
+impl<T> NotifierChain<T> {
     pub fn new() -> Self {
         Self(vec![])
     }
 
     /// @brief 将节点注册到通知链
-    /// @param unique_priority 检查优先级的唯一性
-    // TODO: 未加入 RCU 锁的操作？
+    /// @param unique_priority 检查通知链中优先级的唯一性
     pub fn register(
         &mut self,
-        block: Arc<NotifierBlock>,
+        block: Arc<dyn NotifierBlock<T>>,
         unique_priority: bool,
     ) -> Result<(), SystemError> {
         let mut index: usize = 0;
@@ -49,17 +37,17 @@ impl NotifierChain {
             if Arc::as_ptr(&block) == Arc::as_ptr(b) {
                 kwarn!(
                     "notifier callback {:?} already registered",
-                    block.notifier_call
+                    Arc::as_ptr(&block)
                 );
                 return Err(SystemError::EEXIST);
             }
 
-            if block.priority > b.priority {
+            if block.priority() > b.priority() {
                 break;
             }
 
             // 优先级唯一性检测
-            if block.priority == b.priority && unique_priority {
+            if block.priority() == b.priority() && unique_priority {
                 return Err(SystemError::EBUSY);
             }
 
@@ -72,113 +60,111 @@ impl NotifierChain {
     }
 
     /// @brief 在通知链中取消注册节点
-    // TODO: 未加入 RCU 锁的操作？
-    pub fn unregister(&mut self, block: Arc<NotifierBlock>) -> Result<(), SystemError> {
-        let mut index: usize = 0;
-
-        // 在 notifier chain 中寻找要删除的节点
-        for b in self.0.iter() {
-            if Arc::as_ptr(&block) == Arc::as_ptr(b) {
-                // 在 notifier chain 中删除
-                self.0.remove(index);
-                return Ok(());
-            }
-            index += 1;
+    pub fn unregister(&mut self, block: Arc<dyn NotifierBlock<T>>) -> Result<(), SystemError> {
+        let remove = self
+            .0
+            .drain_filter(|b| Arc::as_ptr(&block) == Arc::as_ptr(b));
+        match remove.count() {
+            0 => return Err(SystemError::ENOENT),
+            _ => return Ok(()),
         }
-        return Err(SystemError::ENOENT);
     }
 
     /// @brief 通知链进行事件通知
     /// @param nr_to_call 回调函数次数，如果该参数小于 0，则忽略
-    /// @param nr_calls 记录回调函数次数，如果该参数为空指针，则忽略
-    /// return 返回最后一次回调函数的返回值
-    // TODO: 未加入 RCU 锁的操作？
+    /// @return (最后一次回调函数的返回值，回调次数)
     // TODO: 增加 NOTIFIER_STOP_MASK 相关功能
-    pub fn call_chain(&self, val: u64, v: *mut c_void, nr_to_call: i32, nr_calls: *mut i32) -> i32 {
-        if !nr_calls.is_null() {
-            unsafe {
-                *nr_calls = 0;
-            }
-        }
-        let mut nr_to_call = nr_to_call;
+    // TODO: 未考虑 nr_to_call 相关操作
+    pub fn call_chain(&self, action: u64, data: &T, nr_to_call: i32) -> (i32, usize) {
         let mut ret: i32 = 0;
+        let mut nr_calls: usize = 0;
 
         for b in self.0.iter() {
-            if nr_to_call == 0 {
-                break;
-            }
-
-            if let Some(notifier_call) = b.notifier_call {
-                ret = notifier_call(b.clone(), val, v);
-            }
-
-            if !nr_calls.is_null() {
-                unsafe {
-                    *nr_calls += 1;
-                }
-            }
-
-            nr_to_call -= 1;
+            ret = b.notifier_call(action, data);
+            nr_calls += 1;
         }
-        return ret;
+        return (ret, nr_calls);
     }
 }
 
 /// @brief 原子的通知链，使用 SpinLock 进行同步
-pub struct AtomicNotifierChain(SpinLock<NotifierChain>);
+pub struct AtomicNotifierChain<T>(SpinLock<NotifierChain<T>>);
 
-impl AtomicNotifierChain {
+impl<T> AtomicNotifierChain<T> {
     pub fn new() -> Self {
-        Self(SpinLock::new(NotifierChain::new()))
+        Self(SpinLock::new(NotifierChain::<T>::new()))
     }
 
     pub fn register(
         &mut self,
-        block: Arc<NotifierBlock>,
+        block: Arc<dyn NotifierBlock<T>>,
         unique_priority: bool,
     ) -> Result<(), SystemError> {
         let mut notifier_chain_guard = self.0.lock();
         return notifier_chain_guard.register(block, unique_priority);
     }
 
-    pub fn unregister(&mut self, block: Arc<NotifierBlock>) -> Result<(), SystemError> {
+    pub fn unregister(&mut self, block: Arc<dyn NotifierBlock<T>>) -> Result<(), SystemError> {
         let mut notifier_chain_guard = self.0.lock();
         return notifier_chain_guard.unregister(block);
     }
 
-    pub fn call_chain(&self, val: u64, v: *mut c_void, nr_to_call: i32, nr_calls: *mut i32) -> i32 {
+    pub fn call_chain(&self, action: u64, data: &T, nr_to_call: i32) -> (i32, usize) {
         let notifier_chain_guard = self.0.lock();
-        return notifier_chain_guard.call_chain(val, v, nr_to_call, nr_calls);
+        return notifier_chain_guard.call_chain(action, data, nr_to_call);
     }
 }
 
 /// @brief 可阻塞的通知链，使用 RwLock 进行同步
-pub struct BlockingNotifierChain(RwLock<NotifierChain>);
+// TODO: 使用 semaphore 封装
+pub struct BlockingNotifierChain<T>(RwLock<NotifierChain<T>>);
 
-impl BlockingNotifierChain {
+impl<T> BlockingNotifierChain<T> {
     pub fn new() -> Self {
-        Self(RwLock::new(NotifierChain::new()))
+        Self(RwLock::new(NotifierChain::<T>::new()))
     }
 
     pub fn register(
         &mut self,
-        block: Arc<NotifierBlock>,
+        block: Arc<dyn NotifierBlock<T>>,
         unique_priority: bool,
     ) -> Result<(), SystemError> {
         let mut notifier_chain_guard = self.0.write();
         return notifier_chain_guard.register(block, unique_priority);
     }
 
-    pub fn unregister(&mut self, block: Arc<NotifierBlock>) -> Result<(), SystemError> {
+    pub fn unregister(&mut self, block: Arc<dyn NotifierBlock<T>>) -> Result<(), SystemError> {
         let mut notifier_chain_guard = self.0.write();
         return notifier_chain_guard.unregister(block);
     }
 
-    pub fn call_chain(&self, val: u64, v: *mut c_void, nr_to_call: i32, nr_calls: *mut i32) -> i32 {
+    pub fn call_chain(&self, action: u64, data: &T, nr_to_call: i32) -> (i32, usize) {
         let notifier_chain_guard = self.0.read();
-        return notifier_chain_guard.call_chain(val, v, nr_to_call, nr_calls);
+        return notifier_chain_guard.call_chain(action, data, nr_to_call);
     }
 }
 
 /// @brief 原始的通知链，由调用者自行考虑同步
-pub struct RawNotifierChain(NotifierChain);
+pub struct RawNotifierChain<T>(NotifierChain<T>);
+
+impl<T> RawNotifierChain<T> {
+    pub fn new() -> Self {
+        Self(NotifierChain::<T>::new())
+    }
+
+    pub fn register(
+        &mut self,
+        block: Arc<dyn NotifierBlock<T>>,
+        unique_priority: bool,
+    ) -> Result<(), SystemError> {
+        return self.0.register(block, unique_priority);
+    }
+
+    pub fn unregister(&mut self, block: Arc<dyn NotifierBlock<T>>) -> Result<(), SystemError> {
+        return self.0.unregister(block);
+    }
+
+    pub fn call_chain(&self, action: u64, data: &T, nr_to_call: i32) -> (i32, usize) {
+        return self.0.call_chain(action, data, nr_to_call);
+    }
+}

6e454c84aba4a51ab80930938d4a7442f8132cc6
LoongGshine <gshine@m.scnu.edu.cn>
2023-08-22 12:50:54 +0800

    移动 select 文件位置

--- kernel/src/filesystem/mod.rs
+++ kernel/src/filesystem/mod.rs
@@ -3,6 +3,5 @@ pub mod fat;
 pub mod mbr;
 pub mod procfs;
 pub mod ramfs;
-pub mod select;
 pub mod sysfs;
 pub mod vfs;
--- kernel/src/filesystem/vfs/mod.rs
+++ kernel/src/filesystem/vfs/mod.rs
@@ -4,6 +4,7 @@ pub mod core;
 pub mod fcntl;
 pub mod file;
 pub mod mount;
+pub mod select;
 pub mod syscall;
 mod utils;
 

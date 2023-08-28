1f8334fa0b5f95ebb2847fe3483e0b4338c84a60
ChiChen <2531693734@qq.com>
2023-08-28 20:39:30 +0800

    temp fix

--- kernel/src/sched/completion.rs
+++ kernel/src/sched/completion.rs
@@ -32,32 +32,75 @@ impl Completion {
         inner.complete_all()
     }
 
+    /// @brief 基本函数：通用的处理wait命令的函数(即所有wait_for_completion函数最核心部分在这里)
+    ///
+    /// @param timeout 非负整数
+    /// @param interuptible 设置进程是否能被打断
+    /// @return 返回剩余时间或者SystemError
+    fn do_wait_for_common(
+        & self,
+        mut timeout: i64,
+        interuptible: bool,
+    ) -> Result<i64, SystemError> {
+        let mut inner = self.inner.lock_irqsave();
+
+        if inner.done == 0 {
+            //loop break 类似 do while 保证进行一次信号检测
+            loop {
+                //检查当前线程是否有未处理的信号
+                //             if (signal_pending_state(state, current)) {
+                // timeout = -ERESTARTSYS;
+                // break;
+                //}
+                // drop(inner);
+                if interuptible {
+                    inner.wait_queue.sleep();
+                } else {
+                    inner.wait_queue.sleep_uninterruptible();
+                }
+                timeout = schedule_timeout(timeout)?;
+                inner = self.inner.lock_irqsave();
+                if inner.done != 0 || timeout <= 0 {
+                    break;
+                }
+            }
+            inner.wait_queue.wakeup(None);
+            if inner.done == 0 {
+                drop(inner);
+                return Ok(timeout);
+            }
+        }
+        if inner.done != COMPLETE_ALL {
+            inner.done -= 1;
+        }
+        drop(inner);
+        return Ok(if timeout > 0 { timeout } else { 1 });
+    }
+
     /// @brief 等待指定时间，超时后就返回, 同时设置pcb state为uninteruptible.
     /// @param timeout 非负整数，等待指定时间，超时后就返回/或者提前done
-    pub fn wait_for_completion_timeout(&mut self, timeout: i64) -> Result<i64, SystemError> {
-        let mut inner = self.inner.lock_irqsave();
-        inner.wait_for_completion_timeout(timeout)
+    pub fn wait_for_completion_timeout(&self, timeout: i64) -> Result<i64, SystemError> {
+        self.do_wait_for_common(timeout, false)
     }
 
     /// @brief 等待completion命令唤醒进程, 同时设置pcb state 为uninteruptible.
-    pub fn wait_for_completion(&mut self) -> Result<i64, SystemError> {
-        let mut inner = self.inner.lock_irqsave();
-        inner.wait_for_completion()
+    pub fn wait_for_completion(&self) -> Result<i64, SystemError> {
+        self.do_wait_for_common(MAX_TIMEOUT, false)
     }
 
     /// @brief @brief 等待completion的完成，但是可以被中断
-    pub fn wait_for_completion_interruptible(&mut self) -> Result<i64, SystemError> {
-        let mut inner = self.inner.lock_irqsave();
-        inner.wait_for_completion_interruptible()
+    pub fn wait_for_completion_interruptible(&self) -> Result<i64, SystemError> {
+        self.do_wait_for_common(MAX_TIMEOUT, true)
     }
 
     pub fn wait_for_completion_interruptible_timeout(
         &mut self,
         timeout: i64,
     ) -> Result<i64, SystemError> {
-        let mut inner = self.inner.lock_irqsave();
-        inner.wait_for_completion_interruptible_timeout(timeout)
+        assert!(timeout >= 0);
+        self.do_wait_for_common(timeout, true)
     }
+
 
     /// @brief @brief 尝试获取completion的一个done！如果您在wait之前加上这个函数作为判断，说不定会加快运行速度。
     ///
@@ -69,7 +112,7 @@ impl Completion {
     }
     // @brief 测试一个completion是否有waiter。（即done是不是等于0）
     pub fn completion_done(&self) -> bool {
-        let mut inner = self.inner.lock_irqsave();
+        let inner = self.inner.lock_irqsave();
         inner.completion_done()
     }
 }
@@ -100,74 +143,6 @@ impl InnerCompletion {
         self.wait_queue.wakeup_all(None);
     }
 
-    /// @brief 基本函数：通用的处理wait命令的函数(即所有wait_for_completion函数最核心部分在这里)
-    ///
-    /// @param timeout 非负整数
-    /// @param interuptible 设置进程是否能被打断
-    /// @return 返回剩余时间或者SystemError
-    fn do_wait_for_common(
-        &mut self,
-        mut timeout: i64,
-        interuptible: bool,
-    ) -> Result<i64, SystemError> {
-        let mut irq_guard = unsafe { CurrentIrqArch::save_and_disable_irq() };
-
-        if self.done == 0 {
-            //loop break 类似 do while 保证进行一次信号检测
-            loop {
-                //检查当前线程是否有未处理的信号
-                //             if (signal_pending_state(state, current)) {
-                // timeout = -ERESTARTSYS;
-                // break;
-                //}
-                drop(irq_guard);
-                if interuptible {
-                    self.wait_queue.sleep();
-                } else {
-                    self.wait_queue.sleep_uninterruptible();
-                }
-                timeout = schedule_timeout(timeout)?;
-                irq_guard = unsafe { CurrentIrqArch::save_and_disable_irq() };
-                if self.done != 0 || timeout <= 0 {
-                    break;
-                }
-            }
-            self.wait_queue.wakeup(None);
-            if self.done == 0 {
-                drop(irq_guard);
-                return Ok(timeout);
-            }
-        }
-        if self.done != COMPLETE_ALL {
-            self.done -= 1;
-        }
-        drop(irq_guard);
-        return Ok(if timeout > 0 { timeout } else { 1 });
-    }
-
-    /// @brief 等待指定时间，超时后就返回, 同时设置pcb state为uninteruptible.
-    /// @param timeout 非负整数，等待指定时间，超时后就返回/或者提前done
-    pub fn wait_for_completion_timeout(&mut self, timeout: i64) -> Result<i64, SystemError> {
-        self.do_wait_for_common(timeout, false)
-    }
-
-    /// @brief 等待completion命令唤醒进程, 同时设置pcb state 为uninteruptible.
-    pub fn wait_for_completion(&mut self) -> Result<i64, SystemError> {
-        self.do_wait_for_common(MAX_TIMEOUT, false)
-    }
-
-    /// @brief @brief 等待completion的完成，但是可以被中断
-    pub fn wait_for_completion_interruptible(&mut self) -> Result<i64, SystemError> {
-        self.do_wait_for_common(MAX_TIMEOUT, true)
-    }
-
-    pub fn wait_for_completion_interruptible_timeout(
-        &mut self,
-        timeout: i64,
-    ) -> Result<i64, SystemError> {
-        assert!(timeout >= 0);
-        self.do_wait_for_common(timeout, true)
-    }
 
     /// @brief @brief 尝试获取completion的一个done！如果您在wait之前加上这个函数作为判断，说不定会加快运行速度。
     ///

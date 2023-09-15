15280822c4cb600dedb081fb8e06eb2b4887087b
longjin <longjin@RinGoTek.cn>
2023-09-15 06:55:56 +0000

    删除C的tss struct

--- kernel/src/process/proc-types.h
+++ kernel/src/process/proc-types.h
@@ -21,29 +21,3 @@
 #define PROC_ZOMBIE (1 << 3)
 // 已停止
 #define PROC_STOPPED (1 << 4)
-
-// 将进程的pcb和内核栈融合到一起,8字节对齐
-union proc_union
-{
-    ul stack[STACK_SIZE / sizeof(ul)];
-} __attribute__((aligned(8)));
-
-struct tss_struct
-{
-    unsigned int reserved0;
-    ul rsp0;
-    ul rsp1;
-    ul rsp2;
-    ul reserved1;
-    ul ist1;
-    ul ist2;
-    ul ist3;
-    ul ist4;
-    ul ist5;
-    ul ist6;
-    ul ist7;
-    ul reserved2;
-    unsigned short reserved3;
-    // io位图基地址
-    unsigned short io_map_base_addr;
-} __attribute__((packed)); // 使用packed表明是紧凑结构，编译器不会对成员变量进行字节对齐。

fdb832d588fbfcb400670eff4f8bd30c91457252
longjin <longjin@DragonOS.org>
2023-11-20 07:09:21 +0000

    调整脚本,使得能够创建riscv的磁盘镜像,并引导进入riscv下的grub
    ```shell
    export ARCH=riscv64
    make write_diskimage
    make qemu
    ```
    
    即可在serial_opt.txt看到进入grub的提示信息

--- tools/umount_virt_disk.sh
+++ tools/umount_virt_disk.sh
@@ -5,6 +5,13 @@ if [ ! $uid == "0" ];then
  exit
 fi
 
+if [ ! ${ARCH} ];then
+ echo "请设置ARCH环境变量"
+ exit
+fi
+
+DISK_NAME=disk-${ARCH}.img
+
 LOOP_DEVICE=$(lsblk | grep disk_mount|sed 's/.*\(loop[0-9]*\)p1.*/\1/1g'|awk 'END{print $0}')
 
 umount -f ../bin/disk_mount/

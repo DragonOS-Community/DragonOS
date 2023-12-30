c338f0546555c9755f67067a7adf0c80f95a8fa4
longjin <longjin@RinGoTek.cn>
2023-12-30 08:22:37 +0000

    fix: 修复安装musl-gcc的脚本没能正确设置x86_64下的环境变量的问题

--- tools/install_musl_gcc.sh
+++ tools/install_musl_gcc.sh
@@ -46,7 +46,7 @@ if [ ! -n "$(which x86_64-linux-musl-gcc)" ] || [ ! -n "$(which x86_64-linux-mus
     echo "下载完成"
     echo "开始解压x86_64-linux-musl-gcc"
     tar xvf $MUSL_GCC_X86_64_TAR -C $INSTALL_POS || exit 1
-    echo "PATH=\$PATH:$INSTALL_POS/x86_64-linux-musl-cross-gcc-${MUSL_GCC_VERSION}-${MUSL_GCC_DATE}/bin" >> $SHELL_RC
+    echo "PATH=\$PATH:$INSTALL_POS/x86_64-linux-musl-cross-gcc-${MUSL_GCC_VERSION}/bin" >> $SHELL_RC
     echo "安装完成"
     echo "开始清理x86_64-linux-musl-gcc的下载缓存"
     rm -rf $MUSL_GCC_X86_64_TAR || exit 1

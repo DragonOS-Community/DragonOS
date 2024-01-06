3b1326be3e70a5ed0e5afd6f3a8e6da0afc9a2a3
longjin <longjin@RinGoTek.cn>
2024-01-06 16:52:36 +0800

    完成bitmap的static bitmap功能，能够静态声明bitmap

--- kernel/crates/bitmap/src/traits.rs
+++ kernel/crates/bitmap/src/traits.rs
@@ -1,9 +1,12 @@
-use core::ops::BitAndAssign;
+use core::ops::{BitAnd, BitAndAssign, BitOrAssign, Not};
 
 /// A trait that defines generalised operations on a `Bits::Store` type.
-pub trait BitOps: BitAndAssign + Sized + Copy {
+pub trait BitOps:
+    BitAndAssign + Sized + Copy + PartialEq + Not + BitOrAssign + BitOrAssign + BitAnd
+{
     fn get(bits: &Self, index: usize) -> bool;
     fn set(bits: &mut Self, index: usize, value: bool) -> bool;
+    fn set_value(bits: &mut Self, value: Self);
     fn len(bits: &Self) -> usize;
     fn first_index(bits: &Self) -> Option<usize>;
     fn first_false_index(bits: &Self) -> Option<usize>;
@@ -19,6 +22,8 @@ pub trait BitOps: BitAndAssign + Sized + Copy {
     fn invert(bits: &mut Self);
     fn make_mask(shift: usize) -> Self;
     fn bit_size() -> usize;
+    fn zero() -> Self;
+    fn max() -> Self;
 }
 
 macro_rules! bitops_for {
@@ -39,6 +44,11 @@ macro_rules! bitops_for {
                     *bits &= !mask;
                 }
                 prev != 0
+            }
+
+            #[inline]
+            fn set_value(bits: &mut Self, value: Self) {
+                *bits = value;
             }
 
             #[inline]
@@ -133,12 +143,12 @@ macro_rules! bitops_for {
                 if *bits == <$target>::MAX || index == 0 {
                     None
                 } else {
-                    let intermediate = bits | (<$target>::MAX.overflowing_shl(index as u32 - 1).0);
+                    let intermediate = bits | (<$target>::MAX.overflowing_shl(index as u32).0);
 
                     if intermediate == <$target>::MAX {
                         None
                     } else {
-                        Some(<$target>::BITS as usize - 1 - (intermediate.leading_zeros() as usize))
+                        Some(<$target>::BITS as usize - 1 - (intermediate.leading_ones() as usize))
                     }
                 }
             }
@@ -165,7 +175,11 @@ macro_rules! bitops_for {
 
             #[inline]
             fn make_mask(shift: usize) -> Self {
-                (1 << shift) - 1
+                if shift == <$target>::BITS as usize {
+                    <$target>::MAX
+                } else {
+                    (1 << shift) - 1
+                }
             }
 
             #[cfg(feature = "std")]
@@ -176,6 +190,16 @@ macro_rules! bitops_for {
             #[inline]
             fn bit_size() -> usize {
                 <$target>::BITS as usize
+            }
+
+            #[inline]
+            fn zero() -> Self {
+                0
+            }
+
+            #[inline]
+            fn max() -> Self {
+                <$target>::MAX
             }
         }
     };
@@ -190,32 +214,101 @@ bitops_for!(usize);
 
 /// Bitmap应当实现的trait
 pub trait BitMapOps<T: BitOps> {
+    /// 获取指定index的位
+    ///
+    /// ## 返回
+    ///
+    /// - `Some(true)` - 该位为1
+    /// - `Some(false)` - 该位为0
+    /// - `None` - index超出范围
     fn get(&self, index: usize) -> Option<bool>;
+
+    /// 设置指定index的位，并返回该位之前的值
+    ///
+    /// ## 参数
+    ///
+    /// - `index` - 位的index
+    /// - `value` - 位的新值
+    ///
+    /// ## 返回
+    ///
+    /// - `Some(true)` - 该位之前为1
+    /// - `Some(false)` - 该位之前为0
+    /// - `None` - index超出范围
     fn set(&mut self, index: usize, value: bool) -> Option<bool>;
+
+    /// 将所有位设置为指定值
+    fn set_all(&mut self, value: bool);
+
     /// 获取bitmap的长度（以位为单位）
     ///
     /// ## Example
     ///
     /// ```
-    /// use bitmap::BitMap;
+    /// use bitmap::StaticBitmap;
+    /// use bitmap::traits::BitMapOps;
     ///
-    /// let mut bitmap = BitMap::<34>::new(10);
+    /// let mut bitmap = StaticBitmap::<34>::new();
     /// assert_eq!(bitmap.len(), 34);
     /// ```
     ///
     fn len(&self) -> usize;
     /// 获取bitmap的大小（以字节为单位）
     fn size(&self) -> usize;
+
+    /// 获取第一个为1的位的index
+    ///
+    /// ## 返回
+    ///
+    /// - `Some(index)` - 第一个为1的位的index
+    /// - `None` - 不存在为1的位
     fn first_index(&self) -> Option<usize>;
+
+    /// 获取第一个为0的位的index
+    ///
+    /// ## 返回
+    ///
+    /// - `Some(index)` - 第一个为0的位的index
+    /// - `None` - 不存在为0的位
     fn first_false_index(&self) -> Option<usize>;
+
+    /// 获取最后一个为1的位的index
+    ///
+    /// ## 返回
+    ///
+    /// - `Some(index)` - 最后一个为1的位的index
+    /// - `None` - 不存在为1的位
     fn last_index(&self) -> Option<usize>;
+
+    /// 获取最后一个为0的位的index
+    ///
+    /// ## 返回
+    ///
+    /// - `Some(index)` - 最后一个为0的位的index
+    /// - `None` - 不存在为0的位
     fn last_false_index(&self) -> Option<usize>;
+
+    /// 获取指定index之后第一个为1的位的index
     fn next_index(&self, index: usize) -> Option<usize>;
+
+    /// 获取指定index之后第一个为0的位的index
     fn next_false_index(&self, index: usize) -> Option<usize>;
+
+    /// 获取指定index之前第一个为1的位的index
     fn prev_index(&self, index: usize) -> Option<usize>;
+
+    /// 获取指定index之前第一个为0的位的index
     fn prev_false_index(&self, index: usize) -> Option<usize>;
+
+    /// 反转bitmap
     fn invert(&mut self);
+
+    /// 判断bitmap是否满了
     fn is_full(&self) -> bool;
+
+    /// 判断bitmap是否为空
     fn is_empty(&self) -> bool;
-    fn as_bytes(&self) -> &[u8];
+
+    /// 将bitmap转换为字节数组
+    unsafe fn as_bytes(&self) -> &[u8];
 }

e97d46e5841bc71abd60419fc0c87ba6ab3b1071
longjin <longjin@DragonOS.org>
2024-01-21 09:42:03 +0000

    1

--- env.mk
+++ env.mk
@@ -12,6 +12,9 @@ endif
 # 设置编译器
 ifeq ($(ARCH), x86_64)
 
+# 如果 x86_64时，DragonOS_GCC 为空，那么设置为默认值
+export DragonOS_GCC?=$(HOME)/opt/dragonos-gcc/gcc-x86_64-unknown-none/bin
+
 export CC=$(DragonOS_GCC)/x86_64-elf-gcc
 export LD=ld
 export AS=$(DragonOS_GCC)/x86_64-elf-as

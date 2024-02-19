989401f4dc025ef291f56afc1f3caecc28549746
yuyi2439 <yuyi2439@qq.com>
2024-02-19 11:01:44 +0800

    1

--- kernel/src/common/errno.h
+++ kernel/src/common/errno.h
@@ -50,94 +50,96 @@
 #define ENOSYS 38           /* 功能不支持 Function not supported. */
 #define ENOTEMPTY 39        /* 目录非空 Directory not empty. */
 #define ELOOP 40            /* 符号链接级别过多 Too many levels of symbolic links. */
-#define ENOMSG 41           /* 没有期待类型的消息 No message of the desired type. */
-#define EIDRM 42            /* 标志符被移除 Identifier removed. */
-#define ECHRNG 43           /* 通道号超出范围 Channel number out of range */
-#define EL2NSYNC 44         /* 二级不同步 Level 2 not synchronized */
-#define EL3HLT 45           /* 三级暂停 Level 3 halted */
-#define EL3RST 46           /* 三级重置 Level 3 reset */
-#define ELNRNG 47           /* 链接号超出范围 Link number out of range */
-#define EUNATCH 48          /* 未连接协议驱动程序 Protocol driver not attached */
-#define ENOCSI 49           /* 没有可用的CSI结构 No CSI structure available */
-#define EL2HLT 50           /* 二级暂停 Level 2 halted */
-#define EBADE 51            /* 无效交换 Invalid exchange */
-#define EBADR 52            /* 无效的请求描述符 Invalid request descriptor */
-#define EXFULL 53           /* 交换满 Exchange full */
-#define ENOANO 54           /* 无阳极 No anode */
-#define EBADRQC 55          /* 请求码无效 Invalid request code */
-#define EBADSLT 56          /* 无效插槽 Invalid slot */
-#define EDEADLOCK 57        /* 资源死锁 Resource deadlock would occur */
-#define EBFONT 58           /* 错误的字体文件格式 Bad font file format */
-#define ENOSTR 59           /* 不是STREAM Not a STREAM */
-#define ENODATA 60          /* 队列头没有可读取的消息 No message is available on the STREAM head read queue. */
-#define ETIME 61            /* 流式ioctl()超时 Stream ioctl() timeout */
-#define ENOSR 62            /* 没有STREAM资源  No STREAM resources. */
-#define ENONET 63           /* 机器不在网络上 Machine is not on the network */
-#define ENOPKG 64           /* 未安装软件包 Package not installed */
-#define EREMOTE 65          /* 远程对象 Object is remote */
-#define ENOLINK 66          /* 保留 Reserved. */
-#define EADV 67             /* 外设错误 Advertise error. */
-#define ESRMNT 68           /* 安装错误 Srmount error */
-#define ECOMM 69            /* 发送时发生通信错误 Communication error on send */
-#define EPROTO 70           /* 协议错误 Protocol error. */
-#define EMULTIHOP 71        /* 保留使用 Reserved. */
-#define EDOTDOT 72          /* RFS特定错误 RFS specific error */
-#define EBADMSG 73          /* 错误的消息 Bad message. */
-#define EOVERFLOW 74        /* 数值过大，产生溢出 Value too large to be stored in data type. */
-#define ENOTUNIQ 75         /* 名称在网络上不是唯一的 Name not unique on network */
-#define EBADFD 76           /* 处于不良状态的文件描述符 File descriptor in bad state */
-#define EREMCHG 77          /* 远程地址已更改 Remote address changed */
-#define ELIBACC 78          /* 无法访问所需的共享库 Can not access a needed shared library */
-#define ELIBBAD 79          /* 访问损坏的共享库 Accessing a corrupted shared library */
-#define ELIBSCN 80          /* a. out中的.lib部分已损坏 .lib section in a.out corrupted */
-#define ELIBMAX 81          /* 尝试链接太多共享库 Attempting to link in too many shared libraries */
-#define ELIBEXEC 82         /* 无法直接执行共享库 Cannot exec a shared library directly */
-#define EILSEQ 83           /* 不合法的字符序列 Illegal byte sequence. */
-#define ERESTART 84         /* 中断的系统调用应该重新启动 Interrupted system call should be restarted */
-#define ESTRPIPE 85         /* 流管道错误 Streams pipe error */
-#define EUSERS 86           /* 用户太多 Too many users */
-#define ENOTSOCK 87         /* 不是一个套接字 Not a socket. */
-#define EDESTADDRREQ 88     /* 需要目标地址 Destination address required. */
-#define EMSGSIZE 89         /* 消息过大 Message too large. */
-#define EPROTOTYPE 90       /* 对于套接字而言，错误的协议 Protocol wrong type for socket. */
-#define ENOPROTOOPT 91      /* 协议不可用 Protocol not available. */
-#define EPROTONOSUPPORT 92  /* 协议不被支持 Protocol not supported. */
-#define ESOCKTNOSUPPORT 93  /* 不支持套接字类型 Socket type not supported */
-#define EOPNOTSUPP 94       /* 套接字不支持该操作 Operation not supported on socket (may be the same value as [ENOTSUP]). */
-#define ENOTSUP 94          /* 不被支持 Not supported (may be the same value as [EOPNOTSUPP]). */
-#define EPFNOSUPPORT 95     /* 不支持协议系列 Protocol family not supported */
-#define EAFNOSUPPORT 96     /* 地址family不支持 Address family not supported. */
-#define EADDRINUSE 97       /* 地址正在被使用 Address in use. */
-#define EADDRNOTAVAIL 98    /* 地址不可用 Address  not available. */
-#define ENETDOWN 99         /* 网络已关闭 Network is down. */
-#define ENETUNREACH 100     /* 网络不可达 Network unreachable. */
-#define ENETRESET 101       /* 网络连接已断开 Connection aborted by network. */
-#define ECONNABORTED 102    /* 连接已断开 Connection aborted. */
-#define ECONNRESET 103      /* 连接被重置 Connection reset. */
-#define ENOBUFS 104         /* 缓冲区空间不足 No buffer space available. */
-#define EISCONN 105         /* 套接字已连接 Socket is connected. */
-#define ENOTCONN 106        /* 套接字未连接 The socket is not connected. */
-#define ESHUTDOWN 107       /* 传输端点关闭后无法发送 Cannot send after transport endpoint shutdown */
-#define ETOOMANYREFS 108    /* 引用太多：无法拼接 Too many references: cannot splice */
-#define ETIMEDOUT 109       /* 连接超时 Connection timed out. */
-#define ECONNREFUSED 110    /* 连接被拒绝 Connection refused. */
-#define EHOSTDOWN 111       /* 主机已关闭 Host is down */
-#define EHOSTUNREACH 112    /* 主机不可达 Host is unreachable. */
-#define EALREADY 113        /* 连接已经在处理 Connection already in progress. */
-#define EINPROGRESS 114     /* 操作正在处理 Operation in progress. */
-#define ESTALE 115          /* 保留 Reserved. */
-#define EUCLEAN 116         /* 结构需要清理 Structure needs cleaning */
-#define ENOTNAM 117         /* 不是XENIX命名类型文件 Not a XENIX named type file */
-#define ENAVAIL 118         /* 没有可用的XENIX信号量 No XENIX semaphores available */
-#define EISNAM 119          /* 是命名类型文件 Is a named type file */
-#define EREMOTEIO 120       /* 远程I/O错误 Remote I/O error */
-#define EDQUOT 121          /* 保留使用 Reserved */
-#define ENOMEDIUM 122       /* 没有找到媒介 No medium found */
-#define EMEDIUMTYPE 123     /* 介质类型错误 Wrong medium type */
-#define ECANCELED 124       /* 操作被取消 Operation canceled. */
-#define ENOKEY 125          /* 所需的密钥不可用 Required key not available */
-#define EKEYEXPIRED 126     /* 密钥已过期 Key has expired */
-#define EKEYREVOKED 127     /* 密钥已被撤销 Key has been revoked */
-#define EKEYREJECTED 128    /* 密钥被服务拒绝 Key has been revoked */
-#define EOWNERDEAD 129      /* 之前的拥有者挂了 Previous owner died. */
-#define ENOTRECOVERABLE 130 /* 状态不可恢复 State not recoverable. */
+#define ENOMSG 42           /* 没有期待类型的消息 No message of the desired type. */
+#define EIDRM 43            /* 标志符被移除 Identifier removed. */
+#define ECHRNG 44           /* 通道号超出范围 Channel number out of range */
+#define EL2NSYNC 45         /* 二级不同步 Level 2 not synchronized */
+#define EL3HLT 46           /* 三级暂停 Level 3 halted */
+#define EL3RST 47           /* 三级重置 Level 3 reset */
+#define ELNRNG 48           /* 链接号超出范围 Link number out of range */
+#define EUNATCH 49          /* 未连接协议驱动程序 Protocol driver not attached */
+#define ENOCSI 50           /* 没有可用的CSI结构 No CSI structure available */
+#define EL2HLT 51           /* 二级暂停 Level 2 halted */
+#define EBADE 52            /* 无效交换 Invalid exchange */
+#define EBADR 53            /* 无效的请求描述符 Invalid request descriptor */
+#define EXFULL 54           /* 交换满 Exchange full */
+#define ENOANO 55           /* 无阳极 No anode */
+#define EBADRQC 56          /* 请求码无效 Invalid request code */
+#define EBADSLT 57          /* 无效插槽 Invalid slot */
+#define EDEADLOCK EDEADLK   /* 资源死锁 Resource deadlock would occur */
+#define EBFONT 59           /* 错误的字体文件格式 Bad font file format */
+#define ENOSTR 60           /* 不是STREAM Not a STREAM */
+#define ENODATA 61          /* 队列头没有可读取的消息 No message is available on the STREAM head read queue. */
+#define ETIME 62            /* 流式ioctl()超时 Stream ioctl() timeout */
+#define ENOSR 63            /* 没有STREAM资源  No STREAM resources. */
+#define ENONET 64           /* 机器不在网络上 Machine is not on the network */
+#define ENOPKG 65           /* 未安装软件包 Package not installed */
+#define EREMOTE 66          /* 远程对象 Object is remote */
+#define ENOLINK 67          /* 保留 Reserved. */
+#define EADV 68             /* 外设错误 Advertise error. */
+#define ESRMNT 69           /* 安装错误 Srmount error */
+#define ECOMM 70            /* 发送时发生通信错误 Communication error on send */
+#define EPROTO 71           /* 协议错误 Protocol error. */
+#define EMULTIHOP 72        /* 保留使用 Reserved. */
+#define EDOTDOT 73          /* RFS特定错误 RFS specific error */
+#define EBADMSG 74          /* 错误的消息 Bad message. */
+#define EOVERFLOW 75        /* 数值过大，产生溢出 Value too large to be stored in data type. */
+#define ENOTUNIQ 76         /* 名称在网络上不是唯一的 Name not unique on network */
+#define EBADFD 77           /* 处于不良状态的文件描述符 File descriptor in bad state */
+#define EREMCHG 78          /* 远程地址已更改 Remote address changed */
+#define ELIBACC 79          /* 无法访问所需的共享库 Can not access a needed shared library */
+#define ELIBBAD 80          /* 访问损坏的共享库 Accessing a corrupted shared library */
+#define ELIBSCN 81          /* a. out中的.lib部分已损坏 .lib section in a.out corrupted */
+#define ELIBMAX 82          /* 尝试链接太多共享库 Attempting to link in too many shared libraries */
+#define ELIBEXEC 83         /* 无法直接执行共享库 Cannot exec a shared library directly */
+#define EILSEQ 84           /* 不合法的字符序列 Illegal byte sequence. */
+#define ERESTART 85         /* 中断的系统调用应该重新启动 Interrupted system call should be restarted */
+#define ESTRPIPE 86         /* 流管道错误 Streams pipe error */
+#define EUSERS 87           /* 用户太多 Too many users */
+#define ENOTSOCK 88         /* 不是一个套接字 Not a socket. */
+#define EDESTADDRREQ 89     /* 需要目标地址 Destination address required. */
+#define EMSGSIZE 90         /* 消息过大 Message too large. */
+#define EPROTOTYPE 91       /* 对于套接字而言，错误的协议 Protocol wrong type for socket. */
+#define ENOPROTOOPT 92      /* 协议不可用 Protocol not available. */
+#define EPROTONOSUPPORT 93  /* 协议不被支持 Protocol not supported. */
+#define ESOCKTNOSUPPORT 94  /* 不支持套接字类型 Socket type not supported */
+#define EOPNOTSUPP 95       /* 套接字不支持该操作 Operation not supported on socket (may be the same value as [ENOTSUP]). */
+#define ENOTSUP EOPNOTSUPP  /* 不被支持 Not supported (may be the same value as [EOPNOTSUPP]). */
+#define EPFNOSUPPORT 96     /* 不支持协议系列 Protocol family not supported */
+#define EAFNOSUPPORT 97     /* 地址family不支持 Address family not supported. */
+#define EADDRINUSE 98       /* 地址正在被使用 Address in use. */
+#define EADDRNOTAVAIL 99   /* 地址不可用 Address  not available. */
+#define ENETDOWN 100        /* 网络已关闭 Network is down. */
+#define ENETUNREACH 101     /* 网络不可达 Network unreachable. */
+#define ENETRESET 102       /* 网络连接已断开 Connection aborted by network. */
+#define ECONNABORTED 103    /* 连接已断开 Connection aborted. */
+#define ECONNRESET 104      /* 连接被重置 Connection reset. */
+#define ENOBUFS 105         /* 缓冲区空间不足 No buffer space available. */
+#define EISCONN 106         /* 套接字已连接 Socket is connected. */
+#define ENOTCONN 107        /* 套接字未连接 The socket is not connected. */
+#define ESHUTDOWN 108       /* 传输端点关闭后无法发送 Cannot send after transport endpoint shutdown */
+#define ETOOMANYREFS 109    /* 引用太多：无法拼接 Too many references: cannot splice */
+#define ETIMEDOUT 110       /* 连接超时 Connection timed out. */
+#define ECONNREFUSED 111    /* 连接被拒绝 Connection refused. */
+#define EHOSTDOWN 112       /* 主机已关闭 Host is down */
+#define EHOSTUNREACH 113    /* 主机不可达 Host is unreachable. */
+#define EALREADY 114        /* 连接已经在处理 Connection already in progress. */
+#define EINPROGRESS 115     /* 操作正在处理 Operation in progress. */
+#define ESTALE 116          /* 保留 Reserved. */
+#define EUCLEAN 117         /* 结构需要清理 Structure needs cleaning */
+#define ENOTNAM 118         /* 不是XENIX命名类型文件 Not a XENIX named type file */
+#define ENAVAIL 119         /* 没有可用的XENIX信号量 No XENIX semaphores available */
+#define EISNAM 120          /* 是命名类型文件 Is a named type file */
+#define EREMOTEIO 121       /* 远程I/O错误 Remote I/O error */
+#define EDQUOT 122          /* 保留使用 Reserved */
+#define ENOMEDIUM 123       /* 没有找到媒介 No medium found */
+#define EMEDIUMTYPE 124     /* 介质类型错误 Wrong medium type */
+#define ECANCELED 125       /* 操作被取消 Operation canceled. */
+#define ENOKEY 126          /* 所需的密钥不可用 Required key not available */
+#define EKEYEXPIRED 127     /* 密钥已过期 Key has expired */
+#define EKEYREVOKED 128     /* 密钥已被撤销 Key has been revoked */
+#define EKEYREJECTED 129    /* 密钥被服务拒绝 Key has been revoked */
+#define EOWNERDEAD 130      /* 之前的拥有者挂了 Previous owner died. */
+#define ENOTRECOVERABLE 131 /* 状态不可恢复 State not recoverable. */
+#define ERFKILL 132         /* 由于射频终止，无法操作 Operation not possible due to RF-kill */
+#define EHWPOISON 132       /*  */

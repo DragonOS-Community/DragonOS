5ab1c29190e52f143cd5fa96016b282c85c3db97
longjin <longjin@DragonOS.org>
2024-02-19 11:46:05 +0000

    添加动态申请的bitmap

--- kernel/crates/bitmap/src/bitmap_core.rs
+++ kernel/crates/bitmap/src/bitmap_core.rs
@@ -2,11 +2,11 @@ use core::{intrinsics::unlikely, marker::PhantomData};
 
 use crate::traits::BitOps;
 
-pub(crate) struct BitMapCore<T: BitOps, const N: usize> {
+pub(crate) struct BitMapCore<T: BitOps> {
     phantom: PhantomData<T>,
 }
 
-impl<T: BitOps, const N: usize> BitMapCore<T, N> {
+impl<T: BitOps> BitMapCore<T> {
     pub const fn new() -> Self {
         Self {
             phantom: PhantomData,
@@ -14,8 +14,8 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
     }
 
     /// 获取位图中的某一位
-    pub(crate) fn get(&self, data: &[T], index: usize) -> Option<bool> {
-        if unlikely(index >= N) {
+    pub(crate) fn get(&self, n: usize, data: &[T], index: usize) -> Option<bool> {
+        if unlikely(index >= n) {
             return None;
         }
 
@@ -29,8 +29,8 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
     }
 
     /// 设置位图中的某一位
-    pub(crate) fn set(&self, data: &mut [T], index: usize, value: bool) -> Option<bool> {
-        if unlikely(index >= N) {
+    pub(crate) fn set(&self, n: usize, data: &mut [T], index: usize, value: bool) -> Option<bool> {
+        if unlikely(index >= n) {
             return None;
         }
         let element_index = index / T::bit_size();
@@ -42,7 +42,7 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
         Some(bit)
     }
 
-    pub(crate) fn set_all(&self, data: &mut [T], value: bool) {
+    pub(crate) fn set_all(&self, n: usize, data: &mut [T], value: bool) {
         let val = if value { T::max() } else { T::zero() };
         for element in data.iter_mut() {
             *element = val;
@@ -50,7 +50,7 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
 
         // 特殊处理最后一个元素
         let last_element = data.last_mut().unwrap();
-        let mask = T::make_mask(N % T::bit_size());
+        let mask = T::make_mask(n % T::bit_size());
         if mask != T::zero() {
             *last_element &= mask;
         }
@@ -69,10 +69,10 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
     }
 
     /// 获取位图中第一个为0的位
-    pub(crate) fn first_false_index(&self, data: &[T]) -> Option<usize> {
+    pub(crate) fn first_false_index(&self, n: usize, data: &[T]) -> Option<usize> {
         for (i, element) in data.iter().enumerate() {
             if let Some(bit) = <T as BitOps>::first_false_index(element) {
-                return self.make_index(i * T::bit_size() + bit);
+                return self.make_index(n, i * T::bit_size() + bit);
             }
         }
 
@@ -80,10 +80,10 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
     }
 
     /// 获取位图中最后一个为1的位
-    pub(crate) fn last_index(&self, data: &[T]) -> Option<usize> {
+    pub(crate) fn last_index(&self, n: usize, data: &[T]) -> Option<usize> {
         for (i, element) in data.iter().enumerate().rev() {
             if let Some(bit) = <T as BitOps>::last_index(element) {
-                return self.make_index(i * T::bit_size() + bit);
+                return self.make_index(n, i * T::bit_size() + bit);
             }
         }
 
@@ -96,12 +96,12 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
     ///
     /// - `data`：位图数据
     /// - `n`：位图有效位数
-    pub(crate) fn last_false_index(&self, data: &[T]) -> Option<usize> {
+    pub(crate) fn last_false_index(&self, n: usize, data: &[T]) -> Option<usize> {
         let mut iter = data.iter().rev();
         let mut last_element = *iter.next()?;
 
         // 对最后一个元素进行特殊处理，因为最后一个元素可能不是满的
-        let mut mask = T::make_mask(N % T::bit_size());
+        let mut mask = T::make_mask(n % T::bit_size());
         if mask != T::zero() {
             <T as BitOps>::invert(&mut mask);
 
@@ -109,12 +109,12 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
         }
 
         if let Some(bit) = <T as BitOps>::last_false_index(&last_element) {
-            return self.make_index((data.len() - 1) * T::bit_size() + bit);
+            return self.make_index(n, (data.len() - 1) * T::bit_size() + bit);
         }
 
         for element in iter {
             if let Some(bit) = <T as BitOps>::last_false_index(element) {
-                return self.make_index((data.len() - 1) * T::bit_size() + bit);
+                return self.make_index(n, (data.len() - 1) * T::bit_size() + bit);
             }
         }
 
@@ -122,8 +122,8 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
     }
 
     /// 获取位图中下一个为1的位
-    pub(crate) fn next_index(&self, data: &[T], index: usize) -> Option<usize> {
-        if unlikely(index >= N) {
+    pub(crate) fn next_index(&self, n: usize, data: &[T], index: usize) -> Option<usize> {
+        if unlikely(index >= n) {
             return None;
         }
 
@@ -132,12 +132,12 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
 
         let element = data.get(element_index)?;
         if let Some(bit) = <T as BitOps>::next_index(element, bit_index) {
-            return self.make_index(element_index * T::bit_size() + bit);
+            return self.make_index(n, element_index * T::bit_size() + bit);
         }
 
         for (i, element) in data.iter().enumerate().skip(element_index + 1) {
             if let Some(bit) = <T as BitOps>::first_index(element) {
-                return self.make_index(i * T::bit_size() + bit);
+                return self.make_index(n, i * T::bit_size() + bit);
             }
         }
 
@@ -145,8 +145,8 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
     }
 
     /// 获取位图中下一个为0的位
-    pub(crate) fn next_false_index(&self, data: &[T], index: usize) -> Option<usize> {
-        if unlikely(index >= N) {
+    pub(crate) fn next_false_index(&self, n: usize, data: &[T], index: usize) -> Option<usize> {
+        if unlikely(index >= n) {
             return None;
         }
 
@@ -155,12 +155,12 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
 
         let element = data.get(element_index)?;
         if let Some(bit) = <T as BitOps>::next_false_index(element, bit_index) {
-            return self.make_index(element_index * T::bit_size() + bit);
+            return self.make_index(n, element_index * T::bit_size() + bit);
         }
 
         for (i, element) in data.iter().enumerate().skip(element_index + 1) {
             if let Some(bit) = <T as BitOps>::first_false_index(element) {
-                return self.make_index(i * T::bit_size() + bit);
+                return self.make_index(n, i * T::bit_size() + bit);
             }
         }
 
@@ -168,8 +168,8 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
     }
 
     /// 获取位图中上一个为1的位
-    pub(crate) fn prev_index(&self, data: &[T], index: usize) -> Option<usize> {
-        if unlikely(index >= N) {
+    pub(crate) fn prev_index(&self, n: usize, data: &[T], index: usize) -> Option<usize> {
+        if unlikely(index >= n) {
             return None;
         }
         let element_index = index / T::bit_size();
@@ -177,37 +177,37 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
 
         let element = data.get(element_index)?;
         if let Some(bit) = <T as BitOps>::prev_index(element, bit_index) {
-            return self.make_index(element_index * T::bit_size() + bit);
+            return self.make_index(n, element_index * T::bit_size() + bit);
         }
 
         for (i, element) in data.iter().enumerate().take(element_index).rev() {
             if let Some(bit) = <T as BitOps>::last_index(element) {
-                return self.make_index(i * T::bit_size() + bit);
+                return self.make_index(n, i * T::bit_size() + bit);
             }
         }
 
         None
     }
 
-    pub(crate) fn prev_false_index(&self, data: &[T], index: usize) -> Option<usize> {
+    pub(crate) fn prev_false_index(&self, n: usize, data: &[T], index: usize) -> Option<usize> {
         let element_index = index / T::bit_size();
         let bit_index = index % T::bit_size();
 
         let element = data.get(element_index)?;
         if let Some(bit) = <T as BitOps>::prev_false_index(element, bit_index) {
-            return self.make_index(element_index * T::bit_size() + bit);
+            return self.make_index(n, element_index * T::bit_size() + bit);
         }
 
         for (i, element) in data.iter().enumerate().take(element_index).rev() {
             if let Some(bit) = <T as BitOps>::last_false_index(element) {
-                return self.make_index(i * T::bit_size() + bit);
+                return self.make_index(n, i * T::bit_size() + bit);
             }
         }
 
         None
     }
 
-    pub(crate) fn invert(&self, data: &mut [T]) {
+    pub(crate) fn invert(&self, n: usize, data: &mut [T]) {
         for element in data.iter_mut() {
             <T as BitOps>::invert(element);
         }
@@ -215,19 +215,19 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
         // 特殊处理最后一个元素
 
         let last_element = data.last_mut().unwrap();
-        let mask = T::make_mask(N % T::bit_size());
+        let mask = T::make_mask(n % T::bit_size());
         if mask != T::zero() {
             *last_element &= mask;
         }
     }
 
-    pub(crate) fn is_full(&self, data: &[T]) -> bool {
+    pub(crate) fn is_full(&self, n: usize, data: &[T]) -> bool {
         let mut iter = data.iter().peekable();
         while let Some(element) = iter.next() {
             if iter.peek().is_none() {
                 // 这是最后一个元素，进行特殊处理
                 let mut element = *element;
-                let mut mask = T::make_mask(N % T::bit_size());
+                let mut mask = T::make_mask(n % T::bit_size());
                 if mask == T::zero() {
                     mask = T::max();
                 }
@@ -256,8 +256,8 @@ impl<T: BitOps, const N: usize> BitMapCore<T, N> {
         return true;
     }
 
-    fn make_index(&self, index: usize) -> Option<usize> {
-        if unlikely(index >= N) {
+    fn make_index(&self, n: usize, index: usize) -> Option<usize> {
+        if unlikely(index >= n) {
             return None;
         }
 

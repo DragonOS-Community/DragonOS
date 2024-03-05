a7b94b2a408f1604b78bd9ee475477fe138171d4
xiaolin2004 <1553367438@qq.com>
2024-03-05 16:59:37 +0800

    Revert "small changes"
    This reverts commit e1991314ce687faa2d652479e8ef64f5bea25fa1.

--- kernel/src/filesystem/vfs/syscall.rs
+++ kernel/src/filesystem/vfs/syscall.rs
@@ -5,10 +5,7 @@ use core::{
 };
 
 use alloc::{
-    borrow::ToOwned,
-    string::{String, ToString},
-    sync::Arc,
-    vec::Vec,
+    borrow::ToOwned, string::{String, ToString}, sync::Arc, vec::Vec
 };
 use system_error::SystemError;
 
@@ -1035,44 +1032,35 @@ impl Syscall {
         data: *const c_void,
     ) -> Result<usize, SystemError> {
         let source = Self::read_from_cstr(source).unwrap();
-
+        
         let target = Self::read_from_cstr(target).unwrap();
-
+        
         let filesystemtype = Self::read_from_cstr(filesystemtype).unwrap();
 
-        kdebug!("input fs is {}", filesystemtype);
+        kdebug!("input fs is {}",filesystemtype);
 
-        Self::mkdir(
-            (target.clone() + &source).as_str(),
-            FileMode::O_PATH_FLAGS.bits().try_into().unwrap(),
-        );
-
+        Self::mkdir((target.clone()+&source).as_str(), FileMode::O_PATH_FLAGS.bits().try_into().unwrap());
+        // (FileMode::O_WRONLY|FileMode::O_RDWR|FileMode::O_ACCMODE|FileMode::O_CREAT|FileMode::O_EXCL|FileMode::O_NOCTTY).bits().try_into().unwrap()
         // bug: when using match , cannot return the right filesystem
-        // let _filesystemtype= RamFS::new();
-        let _filesystemtype = match filesystemtype.as_str() {
-            "ramfs" => Ok(RamFS::new()),
-            _ => Err(SystemError::EUNSUPFS),
-        };
+        let _filesystemtype= RamFS::new();
 
-        return Vcore::do_mount(
-            _filesystemtype.unwrap(),
-            (target.clone() + &source).as_str(),
-        );
+        return Vcore::do_mount(_filesystemtype, (target.clone()+&source).as_str());
     }
 
     pub fn read_from_cstr(source: *const c_char) -> Result<String, SystemError> {
-        // Create a CString from the raw pointer
-        let cstr = unsafe { CStr::from_ptr(source) };
+    // Create a CString from the raw pointer
+    let cstr = unsafe { CStr::from_ptr(source) };
 
-        // Convert the CString to a Rust String
-        let result = cstr.to_string_lossy().into_owned();
+    // Convert the CString to a Rust String
+    let result = cstr.to_string_lossy().into_owned();
 
-        if result.len() >= MAX_PATHLEN {
-            return Err(SystemError::ENAMETOOLONG);
-        }
-
-        Ok(result)
+    if result.len() >= MAX_PATHLEN {
+        return Err(SystemError::ENAMETOOLONG);
     }
+
+    Ok(result)
+    }
+    
 }
 
 #[repr(C)]

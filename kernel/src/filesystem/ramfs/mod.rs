8622ab724d69997932618237b676f1a05b1f7569
Samuel Dai <samuka007@qq.com>
2024-03-28 21:02:16 +0800

    完善ramfs move_to (#1)
    * 完善ramfs move_to
    
    * update

--- kernel/src/filesystem/ramfs/mod.rs
+++ kernel/src/filesystem/ramfs/mod.rs
@@ -9,7 +9,7 @@ use crate::{
     filesystem::vfs::{core::generate_inode_id, FileType},
     ipc::pipe::LockedPipeInode,
     libs::spinlock::{SpinLock, SpinLockGuard},
-    time::TimeSpec,
+    time::TimeSpec, libs::casting::DowncastArc
 };
 
 use alloc::{
@@ -411,25 +411,30 @@ impl IndexNode for LockedRamFSInode {
         target: &Arc<dyn IndexNode>,
         new_name: &str,
     ) -> Result<(), SystemError> {
-        let old_inode: Arc<dyn IndexNode> = self.find(old_name)?;
+        let inode: Arc<dyn IndexNode> = self.find(old_name)?;
+        // 修改其对父节点的引用
+        inode
+            .downcast_ref::<LockedRamFSInode>()
+            .ok_or(SystemError::EPERM)?
+            .0
+            .lock()
+            .parent 
+        = Arc::downgrade(&target
+            .clone()
+            .downcast_arc::<LockedRamFSInode>()
+            .ok_or(SystemError::EPERM)?
+        );
 
         // 在新的目录下创建一个硬链接
-        target.link(new_name, &old_inode)?;
-        // 取消现有的目录下的这个硬链接
-        if let Err(err) = self.unlink(old_name) {
-            // 如果取消失败，那就取消新的目录下的硬链接
-            target.unlink(new_name)?;
-            return Err(err);
-        }
-        //转型为&LockedRamFSInode类型
-        let target = target.clone().downcast_arc::<LockedRamFSInode>().unwrap();
-        let old_inode: &LockedRamFSInode = old_inode
-            .downcast_ref::<LockedRamFSInode>()
-            .ok_or(SystemError::EPERM)?;
+        target.link(new_name, &inode)?;
 
-        //修改其对父节点的引用
-        let mut old_inode = old_inode.0.lock();
-        old_inode.parent = Arc::downgrade(&target);
+        // 取消现有的目录下的这个硬链接
+        if let Err(e) = self.unlink(old_name) {
+            // 当操作失败时回退操作
+            target.unlink(new_name)?;
+            return Err(e);
+        }
+
         return Ok(());
     }
 

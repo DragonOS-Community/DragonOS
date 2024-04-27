db83fc0785a8a6228ec3a372733696b3b472e630
hmt <1037827920@qq.com>
2024-04-27 19:01:41 +0800

    配置改为编译x86架构

--- env.mk
+++ env.mk
@@ -2,7 +2,7 @@
 ifeq ($(ARCH), )
 # ！！！！在这里设置ARCH，可选 x86_64 和 riscv64
 # !!!!!!!如果不同时调整这里以及vscode的settings.json，那么自动补全和检查将会失效
-export ARCH?=riscv64
+export ARCH?=x86_64
 endif
 
 ifeq ($(EMULATOR), )

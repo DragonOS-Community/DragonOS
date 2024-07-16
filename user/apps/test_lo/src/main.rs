8dd204bacb961a514990e292002243e7f34e55e3
smallc <2628035541@qq.com>
2024-07-16 20:25:53 +0800

    改用udp测试

--- user/apps/test_lo/src/main.rs
+++ user/apps/test_lo/src/main.rs
@@ -1,25 +1,12 @@
-use icmp;
-use std::net::{IpAddr, Ipv4Addr};
+use std::net::UdpSocket;
+use std::thread;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
-    let localhost_v4 = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
-    let ping = icmp::IcmpSocket::connect(localhost_v4);
-    let mut ping = ping.unwrap();
-
-    let payload: &[u8] = &[1, 2];
-
-    let result = ping.send(payload);
-    assert_eq!(result.unwrap(), 2);
-
-    let mut buffer = [0; 1024]; // 创建一个缓冲区来存储响应数据
-    let recv_result = ping.recv(&mut buffer); // 接收响应数据
-
-    match recv_result {
-        Ok(size) => {
-            println!("Received {} bytes: {:?}", size, &buffer[..size]);
-        }
-        Err(e) => {
-            println!("Failed to receive response: {}", e);
-        }
-    }
+    // 在主线程中发送数据
+    let socket = UdpSocket::bind("127.0.0.1:8083").expect("could not bind to address");
+    socket
+        .send_to(&[1; 10], "127.0.0.1:8082")
+        .expect("couldn't send data");
 }

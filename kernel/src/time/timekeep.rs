af097f9f4b317337fe74aaa5070c34a14b8635fd
黄铭涛 <114841534+1037827920@users.noreply.github.com>
2024-07-16 21:51:21 +0800

    fix(time): modify update wall time (#836)
    更改了时间子系统的update_wall_time函数，通过读取当前周期数，计算delta值进行更新，而不是通过传入delta值进行更新

--- kernel/src/time/timekeep.rs
+++ kernel/src/time/timekeep.rs
@@ -1,10 +1,16 @@
 #![allow(dead_code)]
 
+use core::intrinsics::unlikely;
+
 use system_error::SystemError;
 
 use crate::driver::rtc::interface::rtc_read_time_default;
 
-use super::PosixTimeSpec;
+use super::{PosixTimeSpec, NSEC_PER_SEC};
+
+// 参考：https://code.dragonos.org.cn/xref/linux-3.4.99/include/linux/time.h#110
+const KTIME_MAX: i64 = !(1u64 << 63) as i64;
+const KTIME_SEC_MAX: i64 = KTIME_MAX / NSEC_PER_SEC as i64;
 
 #[allow(non_camel_case_types)]
 pub type ktime_t = i64;
@@ -30,4 +36,26 @@ fn ktime_get_real() -> Result<ktime_t, SystemError> {
 pub fn ktime_get_real_ns() -> i64 {
     let kt: ktime_t = ktime_get_real().unwrap_or(0);
     return ktime_to_ns(kt);
+}
+
+// # 用于将两个ktime_t类型的变量相加
+// #[inline(always)]
+// pub(super) fn ktime_add(add1: ktime_t, add2: ktime_t) -> ktime_t {
+//     let res = add1 + add2;
+// }
+
+/// # 通过sec和nsec构造一个ktime_t
+#[inline(always)]
+fn ktime_set(secs: i64, nsecs: u64) -> ktime_t {
+    if unlikely(secs >= KTIME_SEC_MAX) {
+        return KTIME_MAX;
+    }
+
+    return secs * NSEC_PER_SEC as i64 + nsecs as i64;
+}
+
+/// # 将PosixTimeSpec转换成ktime_t
+#[inline(always)]
+pub fn timespec_to_ktime(ts: PosixTimeSpec) -> ktime_t {
+    return ktime_set(ts.tv_sec, ts.tv_nsec as u64);
 }

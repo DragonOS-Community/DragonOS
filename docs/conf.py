ef2a79be603d758f104482c0a7fe3d4d79f53dc3
LoGin <longjin@DragonOS.org>
2024-07-17 13:54:28 +0800

    chore: 更新sphinx相关配置，适应read the docs的更新 (#850)
    根据read the docs在7月15日blog,进行此修改
    
    https://about.readthedocs.com/blog/2024/07/addons-by-default/

--- docs/conf.py
+++ docs/conf.py
@@ -10,15 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
 
 # -- Project information -----------------------------------------------------
 
 project = 'DragonOS'
-copyright = '2022-2023, DragonOS Community'
+copyright = '2022-2024, DragonOS Community'
 author = 'longjin'
 
 # The full version, including alpha/beta/rc tags
@@ -74,3 +73,11 @@ myst_enable_extensions = [
     "substitution",
     "tasklist",
 ]
+
+
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    html_context["READTHEDOCS"] = True

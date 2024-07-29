b265c74cbe388f10696316330caf45d005a6fc9b
val213 <1042594899@qq.com>
2024-07-29 23:08:45 +0800

    updates test-uevent

--- user/apps/test-uevent/src/main.rs
+++ user/apps/test-uevent/src/main.rs
@@ -1,25 +1,28 @@
-use netlink_sys::{protocols::{NETLINK_KOBJECT_UEVENT}, Socket, SocketAddr};
-use std::os::unix::io::AsRawFd;
-use nix::sys::socket::{recv, send, MsgFlags};
+use netlink_sys::{Socket, protocols::NETLINK_KOBJECT_UEVENT};
+use std::io::{self, Write};
 
 fn main() {
-    // 创建netlink socket
+    // 创建一个 Netlink 套接字
     let mut socket = Socket::new(NETLINK_KOBJECT_UEVENT).expect("Failed to create netlink socket");
-    // fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_GENERIC);
-    // 绑定到内核
-    let addr = SocketAddr::new(0, 0);
-    socket.bind(&addr).expect("Failed to bind socket");
 
-    // 准备要发送的数据
-    let msg = b"Hello from user space!";
-    
-    // 发送数据到内核
-    send(socket.as_raw_fd(), msg, MsgFlags::empty()).expect("Failed to send message");
+    // 绑定套接字
+    socket.bind_auto().expect("Failed to bind socket");
 
-    // 接收来自内核的数据
+    println!("Listening for uevents...");
+
+    // 接收消息
     let mut buf = vec![0; 4096];
-    let size = recv(socket.as_raw_fd(), &mut buf, MsgFlags::empty()).expect("Failed to receive message");
-
-    // 输出接收到的数据
-    println!("Received message: {}", String::from_utf8_lossy(&buf[..size]));
+    loop {
+        match socket.recv(&mut buf, 0) {
+            Ok(size) => {
+                // 打印接收到的消息
+                io::stdout().write_all(&buf[..size]).expect("Failed to write to stdout");
+                println!();
+            }
+            Err(e) => {
+                eprintln!("Failed to receive message: {}", e);
+                break;
+            }
+        }
+    }
 }

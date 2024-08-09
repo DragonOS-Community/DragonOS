f956080e493b642b8a0c87e2770ca57c91db5639
SMALLC <121806694+smallcjy@users.noreply.github.com>
2024-08-09 13:41:30 +0800

    user(app): 初步实现rust ping 用户程序 (#879)
    * 修复mprotect系统调用未正确设置vm_flags的错误 (#847)
    
    * fix(time): modify update wall time (#836)
    
    更改了时间子系统的update_wall_time函数，通过读取当前周期数，计算delta值进行更新，而不是通过传入delta值进行更新
    
    * chore: 调整triagebot.toml以适应新的组织架构 (#848)
    
    * doc: 完善README.md (#849)
    
    * doc: 完善README.md
    
    * chore: 更新sphinx相关配置，适应read the docs的更新 (#850)
    
    根据read the docs在7月15日blog,进行此修改
    
    https://about.readthedocs.com/blog/2024/07/addons-by-default/
    
    * feat(driver/net): 实现Loopback网卡接口 (#845)
    
    * 初步实现loopback设备
    
    * fix: build-scripts和tools目录下的make check指定工具链版本 (#861)
    
    * fix: tcp poll没有正确处理posix socket的listen状态的问题 (#859)
    
    * chore: 将工具链更新到2024-07-23 (#864)
    
    * chore: 将工具链更新到2024-07-23
    
    * feat(fs): add eventfd syscall support (#858)
    
    * feat(fs): add eventfd syscall support
    
    * refactor: 删除过时的va-pa转换函数，改为统一使用MMArch (#862)
    
    * 默认nightly-2024-07-23 & config改为config.toml (#872)
    
    * fix: 修复由于升级到2024-07-23工具链之后，某些机器上面内核运行一直fault的问题。 (#870)
    
    * fix: 修复由于升级到2024-07-23工具链之后，某些机器上面内核运行一直fault的问题。
    
    * 实现多线程发送ping用户程序
    
    * BUGREPORT: 参数parsing发生错误
    
    * make fmt
    
    * 固定ip
    
    * feat(user): 实现多线程ping用户程序
    
    * 添加注释
    
    * 把ping加载在usr下
    
    * feat(cred): 初步实现Cred (#846)
    
    * 初步实现Cred
    
    * 添加seteuid和setegid
    
    * 添加cred测试程序
    
    * 修改Cred::fscmp返回结果为CredFsCmp枚举
    
    * 完善root用户相关信息
    
    * 多线程有问题，先改成单线程的
    
    * fix: 无法ping本地主机
    
    * feat: 实现多线程ping
    
    * 加注释
    
    ---------
    
    Co-authored-by: MemoryShore <105195940+MemoryShore@users.noreply.github.com>
    Co-authored-by: 黄铭涛 <114841534+1037827920@users.noreply.github.com>
    Co-authored-by: LoGin <longjin@DragonOS.org>
    Co-authored-by: linfeng <1925466036@qq.com>
    Co-authored-by: Jomo <xuzihao@dragonos.org>

--- user/apps/test_lo/src/main.rs
+++ user/apps/test_lo/src/main.rs
@@ -2,26 +2,43 @@ use std::net::UdpSocket;
 use std::str;
 
 fn main() -> std::io::Result<()> {
-    let socket = UdpSocket::bind("10.0.2.15:34254")?;
-    socket.connect("127.0.0.2:34255")?;
-
-    let listener = UdpSocket::bind("127.0.0.2:34255")?;
-
-    let msg = "Hello, loopback!";
-    socket.send(msg.as_bytes())?;
+    let listener = UdpSocket::bind("0.0.0.0:12580")?;
 
     let mut buf = [0; 1024];
-    let (amt, _src) = listener.recv_from(&mut buf)?;
+    let (amt, src) = listener.recv_from(&mut buf)?;
 
     let received_msg = str::from_utf8(&buf[..amt]).expect("Could not read buffer as UTF-8");
 
-    println!("Sent: {}", msg);
-    println!("Received: {}", received_msg);
+    println!("{:?}",  src);
 
-    assert_eq!(
-        msg, received_msg,
-        "The sent and received messages do not match!"
-    );
+    listener.send_to(b"Hello, DragonOS", src)?;
+
+    println!("Received: {}", received_msg);
 
     Ok(())
 }
+
+// use std::io::prelude::*;
+// use std::net::TcpListener;
+// use std::net::TcpStream;
+
+// fn main() -> std::io::Result<()> {
+//     let listener = TcpListener::bind("0.0.0.0:12580")?;
+
+//     for stream in listener.incoming() {
+//         handle_client(stream?);
+//     }
+
+//     Ok(())
+// }
+
+// fn handle_client(mut stream: TcpStream) {
+//     let mut buffer = [0; 512];
+//     stream.read(&mut buffer).unwrap();
+
+//     println!("Recv message: {}", String::from_utf8_lossy(&buffer[..]));
+//     // 回复一条消息
+//     let response = "Hello I am DragonOS!";
+//     stream.write(response.as_bytes()).unwrap();
+//     stream.flush().unwrap();
+// }

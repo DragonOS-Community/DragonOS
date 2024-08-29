b692ba47c29648c1377d5bd5e885b47ba95abf9e
Samuka007 <samuka007@dragonos.org>
2024-08-29 11:26:13 +0000

    feat: define socket trait, move unused definitions into socket/define

--- kernel/src/net/mod.rs
+++ kernel/src/net/mod.rs
@@ -4,11 +4,8 @@ use core::{
 };
 
 use alloc::{collections::BTreeMap, sync::Arc};
-use socket::netlink::endpoint::NetlinkEndpoint;
-use socket::Socket;
 
 use crate::{driver::net::Iface, libs::rwlock::RwLock};
-use smoltcp::wire::IpEndpoint;
 
 pub mod event_poll;
 pub mod net_core;
@@ -26,41 +23,4 @@ lazy_static! {
 pub fn generate_iface_id() -> usize {
     static IFACE_ID: AtomicUsize = AtomicUsize::new(0);
     return IFACE_ID.fetch_add(1, core::sync::atomic::Ordering::SeqCst);
-}
-
-#[derive(Debug, Clone)]
-pub enum Endpoint {
-    /// 链路层端点
-    LinkLayer(LinkLayerEndpoint),
-    /// 网络层端点
-    Ip(IpEndpoint),
-    /// inode端点
-    Inode(Arc<socket::Inode>),
-    // todo: 增加NetLink机制后，增加NetLink端点
-    /// NetLink端点
-    Netlink(Option<NetlinkEndpoint>),
-}
-
-/// @brief 链路层端点
-#[derive(Debug, Clone)]
-pub struct LinkLayerEndpoint {
-    /// 网卡的接口号
-    pub interface: usize,
-}
-
-impl LinkLayerEndpoint {
-    /// @brief 创建一个链路层端点
-    ///
-    /// @param interface 网卡的接口号
-    ///
-    /// @return 返回创建的链路层端点
-    pub fn new(interface: usize) -> Self {
-        Self { interface }
-    }
-}
-
-impl From<IpEndpoint> for Endpoint {
-    fn from(endpoint: IpEndpoint) -> Self {
-        Self::Ip(endpoint)
-    }
 }

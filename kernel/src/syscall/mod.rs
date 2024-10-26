55e6f0b65f91b32638fd56581f711a816eccdcd1
火花 <sparkhhhhhhhhhh@outlook.com>
2024-10-26 18:13:25 +0800

    feat:添加chown系统调用 (#962)
    * 添加chown系统调用
    
    ---------
    
    Co-authored-by: sparkzky <sparkhhhhhhhhh@outlook.com>
    Co-authored-by: longjin <longjin@DragonOS.org>

--- kernel/src/syscall/mod.rs
+++ kernel/src/syscall/mod.rs
@@ -997,8 +997,32 @@ impl Syscall {
             }
 
             SYS_FCHOWN => {
-                warn!("SYS_FCHOWN has not yet been implemented");
-                Ok(0)
+                let dirfd = args[0] as i32;
+                let uid = args[1];
+                let gid = args[2];
+                Self::fchown(dirfd, uid, gid)
+            }
+            #[cfg(target_arch = "x86_64")]
+            SYS_CHOWN => {
+                let pathname = args[0] as *const u8;
+                let uid = args[1];
+                let gid = args[2];
+                Self::chown(pathname, uid, gid)
+            }
+            #[cfg(target_arch = "x86_64")]
+            SYS_LCHOWN => {
+                let pathname = args[0] as *const u8;
+                let uid = args[1];
+                let gid = args[2];
+                Self::lchown(pathname, uid, gid)
+            }
+            SYS_FCHOWNAT => {
+                let dirfd = args[0] as i32;
+                let pathname = args[1] as *const u8;
+                let uid = args[2];
+                let gid = args[3];
+                let flag = args[4] as i32;
+                Self::fchownat(dirfd, pathname, uid, gid, flag)
             }
 
             SYS_FSYNC => {

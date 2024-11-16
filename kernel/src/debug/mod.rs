750b3b5d91f1d52e6c20dd5d752a0af861ae4da1
linfeng <chenlinfeng25@outlook.com>
2024-11-16 16:21:20 +0800

    feat(debug)[WIP]: add static-keys support (#1025)
    * feat: add static-keys support

--- kernel/src/debug/mod.rs
+++ kernel/src/debug/mod.rs
@@ -1,2 +1,3 @@
+pub mod jump_label;
 pub mod klog;
 pub mod kprobe;
--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -17,6 +17,7 @@
 #![feature(sync_unsafe_cell)]
 #![feature(vec_into_raw_parts)]
 #![feature(c_variadic)]
+#![feature(asm_goto)]
 #![cfg_attr(target_os = "none", no_std)]
 #![allow(static_mut_refs, non_local_definitions, internal_features)]
 // clippy的配置

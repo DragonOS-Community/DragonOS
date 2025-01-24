8c77a9f4c1387636b772a2acaa7f4d304020fcad
LINGLUO00 <3425375449@qq.com>
2025-01-24 14:40:23 +0800

    draft pr commit

--- kernel/src/net/socket/unix/seqpacket/mod.rs
+++ kernel/src/net/socket/unix/seqpacket/mod.rs
@@ -300,6 +300,7 @@ impl Socket for SeqpacketSocket {
             return Ok(());
         }
         // TODO: 释放INODE_MAP相关资源
+        // Gonna fix by YL
 
         // 尝试释放相关抽象地址资源
         let _ = remove_abs_addr(&path);

a239d8576802fd98e4375eb2e83044aa605d9904
sparkzky <sparkhhhhhhhhhh@outlook.com>
2025-03-25 20:23:44 +0800

    fmt

--- kernel/src/process/fork.rs
+++ kernel/src/process/fork.rs
@@ -592,7 +592,7 @@ impl ProcessManager {
         guard.set_pgid(pg.pgid());
         drop(guard);
         //todo 这里应该解除注释，但是每次一到这里就触发调度，然后由于当前进程持有锁的数量不等于0导致panic
-        // 
+        //
         // if let Some(session) = pg.session() {
         //     guard.set_sid(session.sid());
         // }

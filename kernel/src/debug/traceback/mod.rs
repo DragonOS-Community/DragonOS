c562b1dcb2887d3a90c2b215c465d05b6eaf5f44
Godones <chenlinfeng25@outlook.com>
2025-03-29 17:56:01 +0800

    fix the panic error for riscv64
    fix the cargo clippy error

--- kernel/src/debug/traceback/mod.rs
+++ kernel/src/debug/traceback/mod.rs
@@ -13,7 +13,9 @@ fn kallsyms_names_index() {}
 #[no_mangle]
 fn kallsyms_names() {}
 
-pub unsafe fn lookup_kallsyms(addr: u64, level: i32) -> Option<()> {
+/// print the func name according to the pc address and
+/// return true if the func is kernel_main
+pub unsafe fn lookup_kallsyms(addr: u64, level: i32) -> bool {
     let sym_names = kallsyms_names as *const u8;
     // 由于符号表使用nm -n生成，因此是按照地址升序排列的，因此可以二分
     let sym_num = kallsyms_num as usize;
@@ -28,10 +30,14 @@ pub unsafe fn lookup_kallsyms(addr: u64, level: i32) -> Option<()> {
             break;
         }
     }
-    return if index < sym_num {
+    let mut is_kernel_main = false;
+    if index < sym_num {
         let sym_name = CStr::from_ptr(sym_names.add(sym_names_index[index] as usize) as _)
             .to_str()
             .unwrap();
+        if sym_name.starts_with("kernel_main") {
+            is_kernel_main = true;
+        }
         println!(
             "[{}] function:{}() \t(+) {:04} address:{:#018x}",
             level,
@@ -39,11 +45,18 @@ pub unsafe fn lookup_kallsyms(addr: u64, level: i32) -> Option<()> {
             addr - kallsyms_address_list[index],
             addr
         );
-        Some(())
     } else {
-        None
+        println!(
+            "[{}] function:unknown \t(+) {:04} address:{:#018x}",
+            level,
+            addr - kallsyms_address_list[sym_num - 1],
+            addr
+        );
     };
+    return is_kernel_main;
 }
+
+/// Get the address of the symbol
 pub unsafe fn addr_from_symbol(symbol: &str) -> Option<u64> {
     let sym_num = kallsyms_num as usize;
     let sym_names = kallsyms_names as *const u8;

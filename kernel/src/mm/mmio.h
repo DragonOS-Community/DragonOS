91a07ed316018f663b8caaa81e03f7fb67140727
Godones <chenlinfeng25@outlook.com>
2025-04-10 17:16:56 +0800

    refacotr: remove all c files
    Use Rust to implement the symbol table generator.
    Delete unused header files and c files
    Adjust the makefile in the debug directory
    
    Signed-off-by: Godones <chenlinfeng25@outlook.com>

--- kernel/src/mm/mmio.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "mm.h"
-
-extern int rs_mmio_create(uint32_t size, uint64_t vm_flags, uint64_t* res_vaddr, uint64_t* res_length);
-extern int rs_mmio_release(uint64_t vaddr, uint64_t length);

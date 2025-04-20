9c76d7dcaf4c9a58cc6987e141e37e0c6818c04e
longjin <longjin@DragonOS.org>
2025-04-20 18:49:57 +0800

    refactor: 移除未使用的导入和函数，优化代码结构
    Signed-off-by: longjin <longjin@DragonOS.org>

--- build-scripts/kernel_build/src/cfiles/arch/loongarch64.rs
+++ build-scripts/kernel_build/src/cfiles/arch/loongarch64.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashSet, path::PathBuf};
 
-use crate::{constant::ARCH_DIR_LOONGARCH64, utils::FileUtils};
+use crate::constant::ARCH_DIR_LOONGARCH64;
 
 use super::CFilesArch;
 
@@ -12,19 +12,7 @@ impl CFilesArch for LoongArch64CFilesArch {
         c.define("__loongarch", None);
     }
 
-    fn setup_global_include_dir(&self, include_dirs: &mut HashSet<PathBuf>) {
-        include_dirs.insert("src/arch/loongarch64/include".into());
-    }
-
-    fn setup_files(&self, _c: &mut cc::Build, files: &mut HashSet<PathBuf>) {
-        // files.insert(PathBuf::from("src/arch/loongarch64/asm/head.S"));
-
-        FileUtils::list_all_files(&arch_path("asm"), Some("c"), true)
-            .into_iter()
-            .for_each(|f| {
-                files.insert(f);
-            });
-    }
+    fn setup_files(&self, _c: &mut cc::Build, _files: &mut HashSet<PathBuf>) {}
 
     fn setup_global_flags(&self, c: &mut cc::Build) {
         // 在这里设置编译器，不然的话vscode的rust-analyzer会报错
@@ -35,6 +23,7 @@ impl CFilesArch for LoongArch64CFilesArch {
     }
 }
 
+#[allow(dead_code)]
 fn arch_path(relative_path: &str) -> PathBuf {
     PathBuf::from(format!("{}/{}", ARCH_DIR_LOONGARCH64, relative_path))
 }

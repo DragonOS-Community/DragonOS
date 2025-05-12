4a7af07fec48a1f775d065f3b1516dbf5e519851
longjin <longjin@DragonOS.org>
2025-05-12 21:47:53 +0800

    refactor(tty): 从PosixTermios移除Debug trait派生
    Signed-off-by: longjin <longjin@DragonOS.org>

--- kernel/src/driver/tty/termios.rs
+++ kernel/src/driver/tty/termios.rs
@@ -38,7 +38,7 @@ pub struct Termios {
     pub output_speed: u32,
 }
 
-#[derive(Clone, Copy, Default, Debug)]
+#[derive(Clone, Copy, Default)]
 pub struct PosixTermios {
     pub c_iflag: u32,
     pub c_oflag: u32,

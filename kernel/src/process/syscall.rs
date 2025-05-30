7c8a3f89f08c4c98231a45d60e4f714e41aa641c
dolzhuying <1240800466@qq.com>
2025-05-30 15:57:44 +0800

    refactor(process):迁移sys_getrlimit sys_prlimit64 系统调用

--- kernel/src/process/syscall.rs
+++ kernel/src/process/syscall.rs
@@ -10,21 +10,14 @@ use system_error::SystemError;
 use super::{
     exec::{load_binary_file, ExecParam, ExecParamFlags},
     fork::{CloneFlags, KernelCloneArgs},
-    resource::{RLimit64, RLimitID},
-    KernelStack, Pid, ProcessManager,
+    KernelStack, ProcessManager,
 };
 use crate::{
-    arch::{interrupt::TrapFrame, CurrentIrqArch, MMArch},
+    arch::{interrupt::TrapFrame, CurrentIrqArch},
     exception::InterruptArch,
-    filesystem::{
-        procfs::procfs_register_pid,
-        vfs::{file::FileDescriptorVec, MAX_PATHLEN},
-    },
+    filesystem::{procfs::procfs_register_pid, vfs::MAX_PATHLEN},
     libs::rand::rand_bytes,
-    mm::{
-        ucontext::{AddressSpace, UserStack},
-        MemoryManagementArch,
-    },
+    mm::ucontext::AddressSpace,
     process::ProcessControlBlock,
     sched::completion::Completion,
     syscall::{
@@ -256,73 +249,6 @@ impl Syscall {
         }
 
         return Ok(pcb.pid().0);
-    }
-
-    /// # 设置资源限制
-    ///
-    /// TODO: 目前暂时不支持设置资源限制，只提供读取默认值的功能
-    ///
-    /// ## 参数
-    ///
-    /// - pid: 进程号
-    /// - resource: 资源类型
-    /// - new_limit: 新的资源限制
-    /// - old_limit: 旧的资源限制
-    ///
-    /// ## 返回值
-    ///
-    /// - 成功，0
-    /// - 如果old_limit不为NULL，则返回旧的资源限制到old_limit
-    ///
-    pub fn prlimit64(
-        _pid: Pid,
-        resource: usize,
-        _new_limit: *const RLimit64,
-        old_limit: *mut RLimit64,
-    ) -> Result<usize, SystemError> {
-        let resource = RLimitID::try_from(resource)?;
-        let mut writer = None;
-
-        if !old_limit.is_null() {
-            writer = Some(UserBufferWriter::new(
-                old_limit,
-                core::mem::size_of::<RLimit64>(),
-                true,
-            )?);
-        }
-
-        match resource {
-            RLimitID::Stack => {
-                if let Some(mut writer) = writer {
-                    let mut rlimit = writer.buffer::<RLimit64>(0).unwrap()[0];
-                    rlimit.rlim_cur = UserStack::DEFAULT_USER_STACK_SIZE as u64;
-                    rlimit.rlim_max = UserStack::DEFAULT_USER_STACK_SIZE as u64;
-                }
-                return Ok(0);
-            }
-
-            RLimitID::Nofile => {
-                if let Some(mut writer) = writer {
-                    let mut rlimit = writer.buffer::<RLimit64>(0).unwrap()[0];
-                    rlimit.rlim_cur = FileDescriptorVec::PROCESS_MAX_FD as u64;
-                    rlimit.rlim_max = FileDescriptorVec::PROCESS_MAX_FD as u64;
-                }
-                return Ok(0);
-            }
-
-            RLimitID::As | RLimitID::Rss => {
-                if let Some(mut writer) = writer {
-                    let mut rlimit = writer.buffer::<RLimit64>(0).unwrap()[0];
-                    rlimit.rlim_cur = MMArch::USER_END_VADDR.data() as u64;
-                    rlimit.rlim_max = MMArch::USER_END_VADDR.data() as u64;
-                }
-                return Ok(0);
-            }
-
-            _ => {
-                return Err(SystemError::ENOSYS);
-            }
-        }
     }
 
     pub fn uname(name: *mut PosixOldUtsName) -> Result<usize, SystemError> {

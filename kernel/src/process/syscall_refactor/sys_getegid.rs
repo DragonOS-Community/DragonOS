07e58624be32795fdec01232254dcfd063904dd3
dolzhuying <1240800466@qq.com>
2025-05-30 14:38:17 +0800

    refactor(process):迁移getegid系统调用

---
+++ kernel/src/process/syscall_refactor/sys_getegid.rs
@@ -0,0 +1,25 @@
+use system_error::SystemError;
+use alloc::vec::Vec;
+use crate::process::ProcessManager;
+use crate::syscall::table::FormattedSyscallParam;
+use crate::syscall::table::Syscall;
+use crate::arch::syscall::nr::SYS_GETEGID;
+
+pub struct SysGetEgid;
+
+impl Syscall for SysGetEgid{
+    fn num_args(&self) -> usize {
+        0
+    }
+
+    fn handle(&self, _args: &[usize], _from_user: bool) -> Result<usize, SystemError> {
+        let pcb = ProcessManager::current_pcb();
+        return Ok(pcb.cred.lock().egid.data());
+    }
+
+    fn entry_format(&self, _args: &[usize]) -> Vec<FormattedSyscallParam> {
+        vec![]
+    }
+}
+
+syscall_table_macros::declare_syscall!(SYS_GETEGID, SysGetEgid);

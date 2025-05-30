dbb65a0206058a1a39e0d30cda352b6e53e31fff
dolzhuying <1240800466@qq.com>
2025-05-30 14:44:44 +0800

    refactor(process):迁移setfsgid系统调用

---
+++ kernel/src/process/syscall_refactor/sys_setfsgid.rs
@@ -0,0 +1,43 @@
+use system_error::SystemError;
+use alloc::vec::Vec;
+use crate::process::ProcessManager;
+use crate::syscall::table::FormattedSyscallParam;
+use crate::syscall::table::Syscall;
+use crate::arch::syscall::nr::SYS_SETFSGID;
+use crate::process::cred::Kgid;
+
+pub struct SysSetFsgid;
+
+impl SysSetFsgid{
+    fn fsgid(args:&[usize])->usize{
+        args[0]
+    }
+}
+
+impl Syscall for SysSetFsgid{
+    fn num_args(&self)->usize{
+        1
+    }
+
+    fn handle(&self, args:&[usize],_from_user:bool)->Result<usize,SystemError>{
+        let fsgid=Self::fsgid(args);
+        let fsgid = Kgid::new(fsgid);
+
+        let pcb = ProcessManager::current_pcb();
+        let mut guard = pcb.cred.lock();
+        let old_fsgid = guard.fsgid;
+
+        if fsgid == guard.gid || fsgid == guard.egid || fsgid == guard.sgid {
+            guard.setfsgid(fsgid.data());
+        }
+
+        Ok(old_fsgid.data())
+        
+    }
+
+    fn entry_format(&self, args: &[usize]) -> Vec<FormattedSyscallParam> {
+        vec![FormattedSyscallParam::new("fsgid", format!("{:#x}", Self::fsgid(args)))]
+    }
+}
+
+syscall_table_macros::declare_syscall!(SYS_SETFSGID, SysSetFsgid);

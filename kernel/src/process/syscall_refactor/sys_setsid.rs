f1c86add76e82278f44e26670e1495c9d199b0c5
dolzhuying <1240800466@qq.com>
2025-05-30 14:47:06 +0800

    refactor(process):迁移setsid系统调用

---
+++ kernel/src/process/syscall_refactor/sys_setsid.rs
@@ -0,0 +1,31 @@
+use system_error::SystemError;
+use alloc::vec::Vec;
+use crate::process::ProcessManager;
+use crate::syscall::table::FormattedSyscallParam;
+use crate::syscall::table::Syscall;
+use crate::arch::syscall::nr::SYS_SETSID;
+
+pub struct SysSetsid;
+
+impl Syscall for SysSetsid{
+    fn num_args(&self) -> usize {
+        0
+    }
+
+    /// # 函数的功能
+    /// 创建新的会话
+    fn handle(&self, _args: &[usize], _from_user: bool) -> Result<usize, SystemError> {
+        let pcb = ProcessManager::current_pcb();
+        let session = pcb.go_to_new_session()?;
+        let mut guard = pcb.sig_info_mut();
+        guard.set_tty(None);
+        Ok(session.sid().into())
+        
+    }
+
+    fn entry_format(&self, _args: &[usize]) -> Vec<FormattedSyscallParam> {
+        vec![]
+    }
+}
+
+syscall_table_macros::declare_syscall!(SYS_SETSID, SysSetsid);

a1e6f6720724cb7c8ddc7ae0fbca828e5de4880d
dolzhuying <1240800466@qq.com>
2025-05-30 14:41:22 +0800

    refactor(process):迁移getppid系统调用

---
+++ kernel/src/process/syscall_refactor/sys_getppid.rs
@@ -0,0 +1,28 @@
+use system_error::SystemError;
+use alloc::vec::Vec;
+use crate::process::ProcessManager;
+use crate::syscall::table::FormattedSyscallParam;
+use crate::syscall::table::Syscall;
+use crate::arch::syscall::nr::SYS_GETPPID;
+
+pub struct SysGetPpid;
+
+impl Syscall for SysGetPpid{
+    fn num_args(&self) -> usize {
+        0
+    }
+
+    /// # 函数的功能
+    /// 获取当前进程的父进程id
+    fn handle(&self, _args: &[usize], _from_user: bool) -> Result<usize, SystemError> {
+        let current_pcb = ProcessManager::current_pcb();
+        return Ok(current_pcb.basic().ppid().into());
+        
+    }
+
+    fn entry_format(&self, _args: &[usize]) -> Vec<FormattedSyscallParam> {
+        vec![]
+    }
+}
+
+syscall_table_macros::declare_syscall!(SYS_GETPPID, SysGetPpid);

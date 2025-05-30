cd4ec405d7bf891ae004c63c4fed208f2d02ed56
dolzhuying <1240800466@qq.com>
2025-05-30 14:39:43 +0800

    refactor(process):迁移getgid系统调用

---
+++ kernel/src/process/syscall_refactor/sys_getgid.rs
@@ -0,0 +1,25 @@
+use system_error::SystemError;
+use alloc::vec::Vec;
+use crate::process::ProcessManager;
+use crate::syscall::table::FormattedSyscallParam;
+use crate::syscall::table::Syscall;
+use crate::arch::syscall::nr::SYS_GETGID;
+
+pub struct SysGetGid;
+
+impl Syscall for SysGetGid{
+    fn num_args(&self) -> usize {
+        0
+    }
+
+    fn handle(&self, _args: &[usize], _from_user: bool) -> Result<usize, SystemError> {
+        let pcb = ProcessManager::current_pcb();
+        return Ok(pcb.cred.lock().gid.data());
+    }
+
+    fn entry_format(&self, _args: &[usize]) -> Vec<FormattedSyscallParam> {
+        vec![]
+    }
+}
+
+syscall_table_macros::declare_syscall!(SYS_GETGID, SysGetGid);

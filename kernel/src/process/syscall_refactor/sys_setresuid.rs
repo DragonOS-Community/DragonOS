41024f3f16fb918f957d7b8e33e5db99e32bf949
dolzhuying <1240800466@qq.com>
2025-05-30 14:46:47 +0800

    refactor(process):迁移setresuid系统调用

---
+++ kernel/src/process/syscall_refactor/sys_setresuid.rs
@@ -0,0 +1,45 @@
+use system_error::SystemError;
+use alloc::vec::Vec;
+use crate::process::ProcessManager;
+use crate::syscall::table::FormattedSyscallParam;
+use crate::syscall::table::Syscall;
+use crate::arch::syscall::nr::SYS_SETRESUID;
+
+pub struct SysSetResUid;
+
+impl SysSetResUid{
+    fn euid(args:&[usize])->usize{
+        args[1] 
+    }
+}
+
+impl Syscall for SysSetResUid{
+    fn num_args(&self)->usize{
+        2  
+    }
+
+    fn handle(&self, args:&[usize],_from_user:bool)->Result<usize,SystemError>{
+        let euid=Self::euid(args);
+        let pcb = ProcessManager::current_pcb();
+        let mut guard = pcb.cred.lock();
+
+        if euid == usize::MAX || (euid == guard.euid.data() && euid == guard.fsuid.data()) {
+            return Ok(0);
+        }
+
+        if euid != usize::MAX {
+            guard.seteuid(euid);
+        }
+
+        let euid = guard.euid.data();
+        guard.setfsuid(euid);
+
+        return Ok(0);
+    }
+
+    fn entry_format(&self, args: &[usize]) -> Vec<FormattedSyscallParam> {
+        vec![FormattedSyscallParam::new("euid", format!("{:#x}", Self::euid(args)))]
+    }
+}
+
+syscall_table_macros::declare_syscall!(SYS_SETRESUID, SysSetResUid);

cf4dd5ff4abdee34fb9a165fd3616472d646462b
mingtaohuang <1037827920@qq.com>
2025-06-14 22:49:25 +0800

    格式化代码

--- kernel/src/mm/mod.rs
+++ kernel/src/mm/mod.rs
@@ -32,8 +32,8 @@ pub mod no_init;
 pub mod page;
 pub mod percpu;
 pub mod syscall;
-pub mod ucontext;
 pub mod truncate;
+pub mod ucontext;
 
 /// 内核INIT进程的用户地址空间结构体（仅在process_init中初始化）
 static mut __IDLE_PROCESS_ADDRESS_SPACE: Option<Arc<AddressSpace>> = None;

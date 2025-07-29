a776892a4be536188a2783a5ae78131b4693624c
longjin <longjin@DragonOS.org>
2025-07-30 03:25:23 +0800

    fix
    Signed-off-by: longjin <longjin@DragonOS.org>

--- kernel/src/process/syscall/sys_setuid.rs
+++ kernel/src/process/syscall/sys_setuid.rs
@@ -1,9 +1,9 @@
 use crate::arch::interrupt::TrapFrame;
 use crate::arch::syscall::nr::SYS_SETUID;
+use crate::process::cred::Cred;
 use crate::process::ProcessManager;
 use crate::syscall::table::FormattedSyscallParam;
 use crate::syscall::table::Syscall;
-use alloc::sync::Arc;
 use alloc::vec::Vec;
 use system_error::SystemError;
 
@@ -25,18 +25,18 @@ impl Syscall for SysSetUid {
         let pcb = ProcessManager::current_pcb();
         let mut guard = pcb.cred.lock();
 
-        let old_cred = guard.clone();
-        let new_cred_mut = Arc::make_mut(&mut guard);
+        let mut new_cred = (**guard).clone();
 
-        if old_cred.uid.data() == 0 {
-            new_cred_mut.setuid(uid);
-            new_cred_mut.seteuid(uid);
-            new_cred_mut.setsuid(uid);
-        } else if uid == old_cred.uid.data() || uid == old_cred.suid.data() {
-            new_cred_mut.seteuid(uid);
+        if guard.uid.data() == 0 {
+            new_cred.setuid(uid);
+            new_cred.seteuid(uid);
+            new_cred.setsuid(uid);
+        } else if uid == guard.uid.data() || uid == guard.suid.data() {
+            new_cred.seteuid(uid);
         } else {
             return Err(SystemError::EPERM);
         }
+        *guard = Cred::new_arc(new_cred);
 
         return Ok(0);
     }

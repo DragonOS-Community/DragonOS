a3f8cc0e3132023e52c0cb9c5aee79056f03706e
yuming <mingjiangyu1@qq.com>
2025-12-18 22:38:10 +0800

    feat(lmbench/test_cases): 每个测例运行前都加载一遍环境变量

--- user/apps/tests/benchmark/lmbench/test_cases/signal_install_lat.sh
+++ user/apps/tests/benchmark/lmbench/test_cases/signal_install_lat.sh
@@ -5,7 +5,10 @@
 
 set -e
 
-
+# 加载环境变量
+SCTIPDIR=$(cd $(dirname ${BASH_SOURCE[0]}) > /dev/null && pwd)
+ENV_PATH="${SCTIPDIR}/../env.sh"
+source ${ENV_PATH}
 
 echo "=== Running signal_install_lat test ==="
 ${LMBENCH_BIN_DIR}/lat_sig -P 1 install

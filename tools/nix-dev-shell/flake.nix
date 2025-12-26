8261f205de1d0caebd7686ecf17816dc21a063b0
Samuka007 <samuka007@dragonos.org>
2025-12-26 15:05:41 +0800

    Support Nix dev shell in Makefiles

--- tools/nix-dev-shell/flake.nix
+++ tools/nix-dev-shell/flake.nix
@@ -7,15 +7,24 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, fenix, flake-utils, ... } @ inputs:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      fenix,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        rust-toolchain = fenix.packages.${system}.fromToolchainFile{
+        rust-toolchain = fenix.packages.${system}.fromToolchainFile {
           file = ../../kernel/rust-toolchain.toml;
           sha256 = "sha256-3JA9u08FrvsLdi5dGIsUeQZq3Tpn9RvWdkLus2+5cHs=";
         };
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           # 基础工具链
           buildInputs = with pkgs; [
@@ -28,11 +37,11 @@
 
           env = {
             LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+            USING_DRAGONOS_NIX_ENV = 1;
           };
 
           # Shell启动脚本
-          shellHook = ''
-          '';
+          shellHook = '''';
         };
 
         # 兼容旧版nix-shell命令

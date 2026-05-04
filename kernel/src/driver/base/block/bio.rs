e42d8824ecc136a17e1954975cd734deb9e1625f
longjin <longjin@DragonOS.org>
2026-05-04 13:58:36 +0800

    1

--- kernel/src/driver/base/block/bio.rs
+++ kernel/src/driver/base/block/bio.rs
@@ -82,7 +82,7 @@ impl BioRequest {
         })
     }
 
-    /// 创建一个 flush 请求
+    /// Create a new flush request.
     pub fn new_flush() -> Arc<Self> {
         Arc::new(Self {
             inner: SpinLock::new(InnerBioRequest {

f2a2d03406f7fad74922150f67cc42ca550b4e04
longjin <longjin@dragonos.org>
2026-05-18 18:09:21 +0000

    fix(net): preserve UDP IPv6 source family
    Signed-off-by: longjin <longjin@dragonos.org>

---
+++ user/apps/tests/dunitest/suites/normal/udp_ipv6_send_semantics.cc
@@ -0,0 +1,72 @@
+#include <gtest/gtest.h>
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <cstring>
+#include <string>
+
+namespace {
+
+class FdGuard {
+  public:
+    explicit FdGuard(int fd = -1) : fd_(fd) {}
+    FdGuard(const FdGuard&) = delete;
+    FdGuard& operator=(const FdGuard&) = delete;
+
+    ~FdGuard() {
+        if (fd_ >= 0) {
+            close(fd_);
+        }
+    }
+
+    int Get() const { return fd_; }
+
+  private:
+    int fd_;
+};
+
+std::string ErrnoString(int err) {
+    return std::to_string(err) + " (" + std::strerror(err) + ")";
+}
+
+sockaddr_in6 MakeIpv6Addr(const char* addr, uint16_t port) {
+    sockaddr_in6 sa {};
+    sa.sin6_family = AF_INET6;
+    sa.sin6_port = htons(port);
+    EXPECT_EQ(inet_pton(AF_INET6, addr, &sa.sin6_addr), 1);
+    return sa;
+}
+
+}  // namespace
+
+TEST(UdpIpv6SendSemantics, UnreachableNativeIpv6DoesNotPanic) {
+    FdGuard fd(socket(AF_INET6, SOCK_DGRAM, 0));
+    ASSERT_GE(fd.Get(), 0) << "socket(AF_INET6, SOCK_DGRAM) failed: " << ErrnoString(errno);
+
+    sockaddr_in6 dst = MakeIpv6Addr("2001:db8::1", 12345);
+    errno = 0;
+    ssize_t ret = sendto(fd.Get(), "test", 4, 0, reinterpret_cast<sockaddr*>(&dst), sizeof(dst));
+
+    EXPECT_EQ(ret, -1);
+    EXPECT_EQ(errno, ENETUNREACH) << "unexpected errno: " << ErrnoString(errno);
+}
+
+TEST(UdpIpv6SendSemantics, UnspecifiedIpv6DestinationUsesIpv6Loopback) {
+    FdGuard fd(socket(AF_INET6, SOCK_DGRAM, 0));
+    ASSERT_GE(fd.Get(), 0) << "socket(AF_INET6, SOCK_DGRAM) failed: " << ErrnoString(errno);
+
+    sockaddr_in6 dst = MakeIpv6Addr("::", 12345);
+    errno = 0;
+    ssize_t ret = sendto(fd.Get(), "test", 4, 0, reinterpret_cast<sockaddr*>(&dst), sizeof(dst));
+
+    EXPECT_EQ(ret, 4) << "sendto(::) failed: " << ErrnoString(errno);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}

db7bbadf5f6567331d353de6ffa7a0e2eb0d21ef
LoGin <longjin@DragonOS.org>
2026-05-19 19:43:28 +0800

    fix(process): avoid panic on dead pid namespace fork (#1919)
    * fix(process): avoid panic on dead pid namespace fork
    
    Signed-off-by: longjin <longjin@dragonos.org>
    
    * fix(process): reject thread clone across pid namespaces
    
    Signed-off-by: longjin <longjin@dragonos.org>
    
    ---------
    
    Signed-off-by: longjin <longjin@dragonos.org>

---
+++ user/apps/tests/dunitest/suites/normal/pid_namespace_fork.cc
@@ -0,0 +1,54 @@
+#include <gtest/gtest.h>
+
+#include <errno.h>
+#include <pthread.h>
+#include <sched.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+namespace {
+
+void* fork_worker(void*) {
+    pid_t pid = fork();
+    if (pid == 0) {
+        _exit(0);
+    }
+    if (pid > 0) {
+        waitpid(pid, nullptr, 0);
+    }
+    return nullptr;
+}
+
+}  // namespace
+
+TEST(PidNamespaceFork, DeadPidNamespaceForChildrenDoesNotPanic) {
+    if (unshare(CLONE_NEWPID) != 0) {
+        GTEST_SKIP() << "unshare(CLONE_NEWPID) failed: " << strerror(errno);
+    }
+
+    pid_t first = fork();
+    ASSERT_GE(first, 0) << strerror(errno);
+    if (first == 0) {
+        _exit(0);
+    }
+    ASSERT_EQ(waitpid(first, nullptr, 0), first) << strerror(errno);
+
+    errno = 0;
+    pid_t second = fork();
+    if (second == 0) {
+        _exit(0);
+    }
+    ASSERT_EQ(second, -1) << "fork unexpectedly succeeded in a dead child PID namespace";
+    EXPECT_EQ(errno, ENOMEM) << "unexpected fork errno: " << strerror(errno);
+
+    pthread_t thread;
+    int rc = pthread_create(&thread, nullptr, fork_worker, nullptr);
+    ASSERT_NE(rc, 0) << "pthread_create unexpectedly succeeded in a dead child PID namespace";
+    EXPECT_EQ(rc, EINVAL) << "unexpected pthread_create error: " << strerror(rc);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
